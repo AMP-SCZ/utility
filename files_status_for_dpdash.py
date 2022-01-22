3ac55b59f3da337e544b4b34c87aa9502d524c9a
Billah, Tashrif <tbillah@bwh.harvard.edu>
2022-01-22 17:35:59 -0500

    add last modified time column

--- files_status_for_dpdash.py
+++ files_status_for_dpdash.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 
-# cleaning up Kevin's work to make DPdash compatible files
+# cleaned up Kevin's work to make DPdash compatible files
+# inserted codes for mtime: the latest time when a subject's data was downloaded
 
 from pathlib import Path
 import pandas as pd
@@ -42,37 +43,33 @@ def get_summary_from_phoenix(phoenix_dir: Path) -> pd.DataFrame:
 
     df['surveys'] = df.p.apply(lambda x: len(list((x / 'surveys').glob('*json'))) > 0)
     df['eeg'] = df.p.apply(lambda x: len(list((x / 'eeg').glob('*zip'))) > 0)
-    # df['eeg_ss'] = df.p.apply(lambda x: (x / 'eeg' / f'{x.name}.Pronet.Run_sheet_eeg.csv').is_file())
-    # df['actigraphy'] = df.p.apply(lambda x: len(list((x / 'actigraphy').glob('*zip'))) > 0)
-    # df['actigraphy_ss'] = df.p.apply(lambda x: (x / 'actigraphy' / f'{x.name}.Pronet.Run_sheet_actigraphy.csv').is_file())
-    # df['mri'] = df.p.apply(lambda x: len([x for x in (x / 'mri').glob('*') if x.is_dir()]) > 0)
-    # df['mri_ss'] = df.p.apply(lambda x: (x / 'mri' / f'{x.name}.Pronet.Run_sheet_mri.csv').is_file())
-    # df['interviews'] = df.p.apply(lambda x: len([x for x in (x / 'interviews').glob('*') if x.is_dir()]) > 0)
-    # df['interviews_ss'] = df.p.apply(lambda x: (x / 'interviews' / f'{x.name}.Pronet.Run_sheet_interviews.csv').is_file())
+    df['eeg_ss'] = df.p.apply(lambda x: (x / 'eeg' / f'{x.name}.Pronet.Run_sheet_eeg.csv').is_file())
+    df['actigraphy'] = df.p.apply(lambda x: len(list((x / 'actigraphy').glob('*zip'))) > 0)
+    df['actigraphy_ss'] = df.p.apply(lambda x: (x / 'actigraphy' / f'{x.name}.Pronet.Run_sheet_actigraphy.csv').is_file())
+    df['mri'] = df.p.apply(lambda x: len([x for x in (x / 'mri').glob('*') if x.is_dir()]) > 0)
+    df['mri_ss'] = df.p.apply(lambda x: (x / 'mri' / f'{x.name}.Pronet.Run_sheet_mri.csv').is_file())
+    df['interviews'] = df.p.apply(lambda x: len([x for x in (x / 'interviews').glob('*') if x.is_dir()]) > 0)
+    df['interviews_ss'] = df.p.apply(lambda x: (x / 'interviews' / f'{x.name}.Pronet.Run_sheet_interviews.csv').is_file())
     
     
-    # df['mtime']= df.p.apply(lambda x: _latest_mtime(x))
-    df['mtime']= df.p.apply(lambda x: '2021-01-21')
+    df['mtime']= df.p.apply(lambda x: _latest_mtime(x))
     
     return df
     
 
 df = get_summary_from_phoenix(pronet_phoenix_dir)
 
+df_pivot = pd.pivot_table(df, index=['subject', 'site', 'mtime'], columns=['level0', 'level1'], fill_value=False).astype(int)
 
-# df_pivot = pd.pivot_table(df, index=['subject', 'site'], columns=['level0', 'level1', 'mtime'], fill_value=False).astype(int)
-df_pivot = pd.pivot_table(df, index=['subject', 'site'], columns=['level0', 'level1'], fill_value=False).astype(int)
-
-
-for (subject, site), row in df_pivot.iterrows():
+for (subject, site, mtime), row in df_pivot.iterrows():
     df_tmp = row.reset_index()
-    # df_tmp.columns = ['datatype', 'level0', 'level1', 'mtime', 'count']
-    # df_tmp_pivot = pd.pivot_table(df_tmp, columns=['datatype', 'level0', 'level1', 'mtime']).reset_index()
     df_tmp.columns = ['datatype', 'level0', 'level1', 'count']
     df_tmp_pivot = pd.pivot_table(df_tmp, columns=['datatype', 'level0', 'level1']).reset_index()
     df_tmp_pivot['col'] = df_tmp_pivot['datatype'] + '_' + df_tmp_pivot['level1'] + '_' + df_tmp_pivot['level0']
     subject_series_tmp = df_tmp_pivot.set_index('col')[0]
     
+    subject_series_tmp['mtime']= mtime
+     
     # https://gist.github.com/tashrifbillah/cea43521588adf127cae79353ae09968
     # suggestion from Tashrif to link outputs to DPdash
     subject_df_tmp = pd.DataFrame({
@@ -86,7 +83,5 @@ for (subject, site), row in df_pivot.iterrows():
     
     out_file_name = f"{site[-2:]}-{subject}-flowcheck-day1to1.csv"
 
-    # break
     subject_df_tmp.to_csv(outdir/out_file_name, index=False)
-    
     
