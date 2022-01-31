8411fde309913b080467a2a5cecb487d78d68bfd
Billah, Tashrif <tbillah@bwh.harvard.edu>
2022-01-31 14:39:56 -0500

    import site and subject level files

--- dpimport_data.sh
+++ dpimport_data.sh
@@ -3,5 +3,15 @@
 cd /data/predict/kcho/flow_test/
 
 source /opt/dpdash/miniconda3/bin/activate && \
+
+# network level files status
 import.py -c /opt/dpdash/dpimport/examples/config.yml "*_status/files-*-flowcheck-day1to9999.csv" && \
-import.py -c /opt/dpdash/dpimport/examples/config.yml files_metadata.csv
+import.py -c /opt/dpdash/dpimport/examples/config.yml files_metadata.csv && \
+
+# site level files status
+import.py -c /opt/dpdash/dpimport/examples/config.yml "*_status/*-flowcheck-day1to9999.csv" && \
+import.py -c /opt/dpdash/dpimport/examples/config.yml "*_status/*_metadata.csv" && \
+
+# subject level files status
+import.py -c /opt/dpdash/dpimport/examples/config.yml "*_status/*-flowcheck-day1to1.csv"
+
