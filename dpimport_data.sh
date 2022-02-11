37d7ca7505da8ae8532c1c7971e9461a11e88a0f
Billah, Tashrif <tbillah@bwh.harvard.edu>
2022-02-10 23:43:25 -0500

    prevent dual import of files-*-flowcheck-day1to9999.csv

--- dpimport_data.sh
+++ dpimport_data.sh
@@ -4,13 +4,12 @@ cd /data/predict/kcho/flow_test/
 
 source /opt/dpdash/miniconda3/bin/activate && \
 
-# network level files status
-import.py -c /opt/dpdash/dpimport/examples/config.yml "*_status/files-*-flowcheck-day1to9999.csv" && \
+# metadata
 import.py -c /opt/dpdash/dpimport/examples/config.yml files_metadata.csv && \
-
-# site level files status
-import.py -c /opt/dpdash/dpimport/examples/config.yml "*_status/*-flowcheck-day1to9999.csv" && \
 import.py -c /opt/dpdash/dpimport/examples/config.yml "*_status/*_metadata.csv" && \
+
+# project level files status
+import.py -c /opt/dpdash/dpimport/examples/config.yml "*_status/files-*-flowcheck-day1to9999.csv" && \
 
 # subject level files status
 import.py -c /opt/dpdash/dpimport/examples/config.yml "*_status/*-flowcheck-day1to1.csv"
