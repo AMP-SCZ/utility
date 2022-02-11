4a66b6f502dd73ed5fdfbcac3300ced65c8a676e
Billah, Tashrif <tbillah@bwh.harvard.edu>
2022-02-11 17:46:03 -0500

    complete hash generator script

--- gen_hash.py
+++ gen_hash.py
@@ -1,13 +1,21 @@
-from hashlib import hash
+#!/usr/bin/env python
+
+from hashlib import sha256
+import sys
+from os.path import abspath
 
 assessment='flowcheck'
 
-with open('combined_metadata.csv') as f:
+with open(abspath(sys.argv[1])) as f:
     ids=f.read().split('\n')
 
-ids= [i for i in ids if i]
-    
+ids= [i for i in ids if i and (i != 'Subject ID,Active,Consent,Study')]
+
+print(ids)
+
 for i in ids:
     e= i.split(',')
     name= e[-1]+ e[0]+ assessment
-    hashes.append(sha256(name.encode()).hexdigest())
+    hash_mongo= sha256(name.encode()).hexdigest()
+
+    print(hash_mongo)
