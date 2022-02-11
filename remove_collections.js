65b64e01fd83c493ecf95115bf7aefddae345ea8
Billah, Tashrif <tbillah@bwh.harvard.edu>
2022-02-11 16:08:59 -0500

    * use concat * add mising )

--- remove_collections.js
+++ remove_collections.js
@@ -1,7 +1,7 @@
-# javascript code executed by mongo interpreter behind admin authentication
+// javascript code executed by mongo interpreter behind admin authentication
 
-# all sites and pseudo sites: PRoNET, PRESCIENT, files
-let sites= ['ME', 'AD', 'CG', 'JE', 'CP', 'BM', 'AM', 'LS', 'GW', 'SG', 'HK', 'LA', 'OR', 'BI', 'NL', 'NC', 'SD', 'CA', 'YA', 'SF', 'PA', 'SI', 'PI', 'NN', 'IR', 'TE', 'GA', 'WU', 'HA', 'MT', 'KC', 'PV', 'MA', 'CM', 'MU', 'SH', 'SL']+ ["ProNET", "PRESCIENT", "files"]
+// all sites and pseudo sites: PRoNET, PRESCIENT, files
+let sites= ['ME', 'AD', 'CG', 'JE', 'CP', 'BM', 'AM', 'LS', 'GW', 'SG', 'HK', 'LA', 'OR', 'BI', 'NL', 'NC', 'SD', 'CA', 'YA', 'SF', 'PA', 'SI', 'PI', 'NN', 'IR', 'TE', 'GA', 'WU', 'HA', 'MT', 'KC', 'PV', 'MA', 'CM', 'MU', 'SH', 'SL'].concat(["ProNET", "PRESCIENT", "files"]);
 
 sites.forEach(s => {
 
@@ -10,10 +10,11 @@ sites.forEach(s => {
     // delete children collections
     let colls= db.toc.find({"study":s});
     let num= colls.length();
+
     for (let i=0; i<num; i++) {
-            print(db[ colls[i].collection ].remove());
+            print(db[ colls[i].collection ].remove({}));
     }
 
     // delete parent collection
     print(db.toc.remove({"study":s}));
-}
+})
