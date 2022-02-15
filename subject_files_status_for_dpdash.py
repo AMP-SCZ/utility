74cc2a338a1da5181f9ad60008eb70771cb365aa
kevin <kevin@kevins-MacBook-Pro.local>
2022-02-14 21:05:03 -0500

    feat: add internal functions to clean up pandas lambdas

--- subject_files_status_for_dpdash.py
+++ subject_files_status_for_dpdash.py
@@ -7,6 +7,9 @@ from pathlib import Path
 import pandas as pd
 import numpy as np
 from datetime import time, timedelta, datetime, date
+from os import stat
+from os.path import isfile
+from datetime import datetime
 
 flow_test_root = Path('/data/predict/kcho/flow_test')
 pronet_phoenix_dir = flow_test_root / 'Pronet/PHOENIX'
@@ -16,20 +19,15 @@ pronet_status_dir = flow_test_root/ 'Pronet_status'
 prescient_status_dir = flow_test_root/ 'Prescient_status'
 
 
-from os import stat
-from os.path import isfile
-from datetime import datetime
+def _latest_mtime(p: Path) -> str:
+    print('Finding modification time of', p)	
 
-def _latest_mtime(p):
-    
-    print('Finding modification time of',p)	
-
-    latest= -1
+    latest = -1
     for file in p.rglob('*'):
         if file.is_file():
-            mtime= file.stat().st_mtime
-            if mtime>latest:
-                latest= mtime
+            mtime = file.stat().st_mtime
+            if mtime > latest:
+                latest = mtime
     
     return datetime.fromtimestamp(latest).strftime('%Y-%m-%d')
 
@@ -45,19 +43,38 @@ def get_summary_from_phoenix(phoenix_dir: Path) -> pd.DataFrame:
     df['level0'] = df.p.apply(lambda x: x.parent.parent.parent.name)
     df['level1'] = df.p.apply(lambda x: x.parent.name)
 
-    df['pronet_surveys'] = df.p.apply(lambda x: len(list((x / 'surveys').glob('*Pronet.json'))) > 0)
-    df['upenn_surveys'] = df.p.apply(lambda x: len(list((x / 'surveys').glob('*UPENN.json'))) > 0)
-    df['eeg'] = df.p.apply(lambda x: len(list((x / 'eeg').glob('*zip'))) > 0)
-    df['eeg_ss'] = df.p.apply(lambda x: (x / 'eeg' / f'{x.name}.Pronet.Run_sheet_eeg.csv').is_file())
-    df['actigraphy'] = df.p.apply(lambda x: len(list((x / 'actigraphy').glob('*zip'))) > 0)
-    df['actigraphy_ss'] = df.p.apply(lambda x: (x / 'actigraphy' / f'{x.name}.Pronet.Run_sheet_actigraphy.csv').is_file())
-    df['mri'] = df.p.apply(lambda x: len([x for x in (x / 'mri').glob('*') if x.is_dir()]) > 0)
-    df['mri_ss'] = df.p.apply(lambda x: (x / 'mri' / f'{x.name}.Pronet.Run_sheet_mri.csv').is_file())
-    df['interviews'] = df.p.apply(lambda x: len([x for x in (x / 'interviews').glob('*') if x.is_dir()]) > 0)
-    df['interviews_ss'] = df.p.apply(lambda x: (x / 'interviews' / f'{x.name}.Pronet.Run_sheet_interviews.csv').is_file())
-    df['mind_phone'] = df.p.apply(lambda x: len( [x for x in (x / 'phone').glob('*_activity_*json')] ))
-    df['mind_audio'] = df.p.apply(lambda x: len( [x for x in (x / 'phone').glob('*_activity_*_sound_*.mp3')] ))
-    df['mind_sensor'] = df.p.apply(lambda x: len( [x for x in (x / 'phone').glob('*_sensor_*json')] ))
+    # surveys
+    df['surveys'] = df.p.apply(
+            lambda x: _is_file(x, 'surveys', '*Pronet.json') or 
+                      _is_file(x, 'surveys', '*.csv')
+            )
+    df['upenn_surveys'] = df.p.apply(
+            lambda x: _is_file(x, 'surveys', '*UPENN.json'))
+
+    # eeg
+    df['eeg'] = df.p.apply(lambda x: _is_file(x, 'eeg', '*zip'))
+    df['eeg_ss'] = df.p.apply(lambda x: _is_scansheet(x, 'eeg'))
+
+    # actigraphy
+    df['actigraphy'] = df.p.apply(lambda x: _is_file(x, 'actigraphy', '*cwa'))
+    df['actigraphy_ss'] = df.p.apply(lambda x: _is_scansheet(x, 'actigraphy'))
+
+    # mri
+    df['mri'] = df.p.apply(
+            lambda x: _is_dir(x, 'mri', '*') or
+                      _is_file(x, 'mri', '*.zip'))
+    df['mri_ss'] = df.p.apply(lambda x: _is_scansheet(x, 'mri'))
+
+    # A/V
+    df['interviews'] = df.p.apply(
+            lambda x: _is_file(x, 'interviews', '*.csv'))
+    df['interviews_ss'] = df.p.apply(lambda x: _is_scansheet(x, 'interviews'))
+
+    # mindlamp
+    df['mind_phone'] = df.p.apply(
+            lambda x: _get_count(x, 'phone', '*_activity_*json'))
+    df['mind_sensor'] = df.p.apply(
+            lambda x: _get_count(x, 'phone', '*_sensor_*json'))
     
     df['mtime']= df.p.apply(lambda x: _latest_mtime(x))
     
@@ -76,7 +93,8 @@ def phoenix_files_status(phoenix_dir, out_dir):
     for (subject, site, mtime), row in df_pivot.iterrows():
         df_tmp = row.reset_index()
         df_tmp.columns = ['datatype', 'level0', 'level1', 'count']
-        df_tmp_pivot = pd.pivot_table(df_tmp, columns=['datatype', 'level0', 'level1']).reset_index()
+        df_tmp_pivot = pd.pivot_table(
+                df_tmp, columns=['datatype', 'level0', 'level1']).reset_index()
         df_tmp_pivot['col'] = df_tmp_pivot['datatype'] + '_' + \
                               df_tmp_pivot['level1'] + '_' + \
                               df_tmp_pivot['level0']
@@ -94,12 +112,42 @@ def phoenix_files_status(phoenix_dir, out_dir):
             'weekday': ''
         })
         
-        subject_df_tmp = pd.concat([subject_df_tmp, pd.DataFrame(subject_series_tmp).T], axis=1)
-        
+        subject_df_tmp = pd.concat(
+                [subject_df_tmp, pd.DataFrame(subject_series_tmp).T],
+                axis=1)
         out_file = f"{site[-2:]}-{subject}-flowcheck-day1to1.csv"
-
         subject_df_tmp.to_csv(out_dir/out_file, index=False)
 
+
+def _get_count(root: Path, subdir: str, pattern: str) -> int:
+    '''get number of files and directories that mattch glob pattern'''
+    return len(list((root / subdir).glob(str)))
+
+
+def _get_dir_count(root: Path, subdir: str, pattern: str) -> int:
+    '''get number of directories that mattch glob pattern'''
+    return len([x for x in list((root / subdir).glob(str)) if x.is_dir()])
+
+
+def _get_file_count(root: Path, subdir: str, pattern: str) -> int:
+    '''get number of files that mattch glob pattern'''
+    return len([x for x in list((root / subdir).glob(str)) if x.is_file()])
+
+
+def _is_file(root: Path, subdir: str, pattern: str) -> int:
+    '''check if there is at least one file matching the given pattern'''
+    return _get_file_count(root, subdir, pattern) > 0
+
+
+def _is_dir(root:Path, subdir: str, pattern: str) -> int:
+    '''check if there is at least one directory matching the pattern'''
+    return _get_dir_count(root, subdir, pattern) > 0
+
+
+def _is_scansheet(root:Path, subdir: str) -> int:
+    '''check if there is a scan sheet for a datatype (subdir)'''
+    return _get_count(
+            root, subdir, f'{root.name}.*.Run_sheet_{subdir}.csv') > 0
 
 
 if __name__=='__main__':
