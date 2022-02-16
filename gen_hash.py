33b17fbd2c849aba75c549ca6033d5fccfbeb4a8
Tashrif Billah <tbillah@bwh.harvard.edu>
2022-02-16 11:50:06 -0500

    add example name to facilitate future manual code execution

--- gen_hash.py
+++ gen_hash.py
@@ -16,6 +16,8 @@ print(ids)
 for i in ids:
     e= i.split(',')
     name= e[-1]+ e[0]+ assessment
+    
+    # example name= "YAYA00015flowcheck"
     hash_mongo= sha256(name.encode()).hexdigest()
 
     print(hash_mongo)
