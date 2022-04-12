8fed573624a0be8caef31200fd1d379b6fc92966
Billah, Tashrif <tbillah@bwh.harvard.edu>
2022-04-12 09:18:36 -0400

    script for cross-VM dpimport

---
+++ dpimport_remote_data.sh
@@ -0,0 +1,35 @@
+#!/usr/bin/env bash
+
+HOST=
+PORT=
+state=
+MONGO_PASS=
+CONFIG=
+
+export PATH=/data/predict/mongodb-linux-x86_64-rhel70-4.4.6/bin:$PATH
+
+
+if [ -z $HOST ] || [ -z $PORT ] || [ -z $state ] || [ -z $MONGO_PASS ] || [ -z $CONFIG ]
+then
+    echo Define state and MONGO_PASS and try again
+    exit 1
+fi
+
+# delete old collections
+mongo --tls --tlsCAFile $state/ssl/ca/cacert.pem --tlsCertificateKeyFile $state/ssl/mongo_client.pem mongodb://dpdash:$MONGO_PASS@$HOST:$PORT/dpdata?authSource=admin /data/predict/utility/remove_studies.js
+
+# import new collections
+source /data/pnl/soft/pnlpipe3/miniconda3/bin/activate && conda activate dpimport \
+
+cd /data/predict/kcho/flow_test/ && \
+
+# metadata
+import.py -c /data/predict/dpimport/examples/$CONFIG files_metadata.csv && \
+import.py -c /data/predict/dpimport/examples/$CONFIG "*_status/*_metadata.csv" && \
+
+# project level files status
+import.py -c /data/predict/dpimport/examples/$CONFIG "*_status/files-*-flowcheck-day1to9999.csv" && \
+
+# subject level files status
+import.py -c /data/predict/dpimport/examples/$CONFIG "*_status/*-flowcheck-day1to1.csv"
+
