2ae97920c2cff411734fdda34fa45e10935a5b7d
Billah, Tashrif <tbillah@bwh.harvard.edu>
2022-04-16 15:48:08 -0400

    cross-server mriqc importer

--- dpimport_mriqc.sh
+++ dpimport_mriqc.sh
@@ -5,16 +5,23 @@
 # base hash
 # 8d051eccc2a98eb4303bc62316124890ed47810c61ccde18b9d772e3ac379543
 
-state=/opt/dpdash/dpstate
+# dpstage.dipr
+HOST=
+PORT=
+state=
+MONGO_PASS=
+CONFIG=
+
+export PATH=/data/predict/mongodb-linux-x86_64-rhel70-4.4.6/bin:$PATH
 
 # remove old data
-mongo --tls --tlsCAFile $state/ssl/ca/cacert.pem --tlsCertificateKeyFile $state/ssl/mongo_client.pem mongodb://dpdash:$MONGO_PASS@`hostname`:27017/dpdata?authSource=admin --eval "db[\"8d051eccc2a98eb4303bc62316124890ed47810c61ccde18b9d772e3ac379543\"].remove({})"
+mongo --tls --tlsCAFile $state/ssl/ca/cacert.pem --tlsCertificateKeyFile $state/ssl/mongo_client.pem mongodb://dpdash:$MONGO_PASS@$HOST:$PORT/dpdata?authSource=admin --eval "db[\"8d051eccc2a98eb4303bc62316124890ed47810c61ccde18b9d772e3ac379543\"].remove({})"
 echo ''
 
-mongo --tls --tlsCAFile $state/ssl/ca/cacert.pem --tlsCertificateKeyFile $state/ssl/mongo_client.pem mongodb://dpdash:$MONGO_PASS@`hostname`:27017/dpdata?authSource=admin --eval "db.toc.remove({"study":\"mriqc\"})"
+mongo --tls --tlsCAFile $state/ssl/ca/cacert.pem --tlsCertificateKeyFile $state/ssl/mongo_client.pem mongodb://dpdash:$MONGO_PASS@$HOST:$PORT/dpdata?authSource=admin --eval "db.toc.remove({"study":\"mriqc\"})"
 echo ''
 
 # import new data
-source /opt/dpdash/miniconda3/bin/activate
+source /data/pnl/soft/pnlpipe3/miniconda3/bin/activate && conda activate dpimport
 cd /data/predict/kcho/flow_test
-import.py -c /opt/dpdash/dpimport/examples/config.yml MRI_ROOT/derivatives/quick_qc/mriqc-combined-mriqc-day1to9999.csv
+import.py -c /data/predict/dpimport/examples/$CONFIG MRI_ROOT/derivatives/quick_qc/mriqc-combined-mriqc-day1to9999.csv
