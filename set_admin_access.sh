c1981e9a46e9f1832432dbb070f16e74e8af2bd4
Billah, Tashrif <tbillah@bwh.harvard.edu>
2022-04-22 15:18:54 -0400

    * introduce MONGO_UID variable * break down long line for easier GitHub viewing

--- set_admin_access.sh
+++ set_admin_access.sh
@@ -6,6 +6,7 @@ PORT=27017
 state=
 MONGO_PASS=
 CONFIG=
+MONGO_UID=${1:-dpdash}
 
 export PATH=/data/predict/mongodb-linux-x86_64-rhel70-4.4.6/bin:$PATH
 
@@ -16,5 +17,10 @@ then
 fi
 
 
-mongo --tls --tlsCAFile $state/ssl/ca/cacert.pem --tlsCertificateKeyFile $state/ssl/mongo_client.pem mongodb://dpdash:$MONGO_PASS@$HOST:$PORT/dpdata?authSource=admin /data/predict/utility/_set_access.js
+mongo --tls --tlsCAFile $state/ssl/ca/cacert.pem \
+--tlsCertificateKeyFile $state/ssl/mongo_client.pem \
+mongodb://dpdash:$MONGO_PASS@$HOST:$PORT/dpdata?authSource=admin \
+--eval "uid=\"$MONGO_UID\"" \
+/data/predict/utility/_set_admin_access.js
+
 
