33fb38975a34bb8e16246a8f3f97c4c43205de5f
Billah, Tashrif <tbillah@bwh.harvard.edu>
2022-04-22 15:15:07 -0400

    use <db> is prohibited in mongo script

--- _set_admin_access.js
+++ _set_admin_access.js
@@ -1,7 +1,13 @@
 // javascript code executed by mongo interpreter behind admin authentication
 
-use dpdata;
+// https://www.mongodb.com/docs/manual/tutorial/write-scripts-for-the-mongo-shell/
+
+// use dpdata;
+db= db.getSiblingDB('dpdata')
 studies= [... new Set(db.toc.find().map(s=> s.study))]
-use dpdmongo;
-db.users.update({"uid":uid}, {$set: {"access": studies}})
+print(studies)
+
+// use dpdmongo;
+db= db.getSiblingDB('dpdmongo')
+print(db.users.update({"uid": uid}, {$set: {"access": studies}}))
 
