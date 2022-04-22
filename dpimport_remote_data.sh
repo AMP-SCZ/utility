19ab55ddfba688c902b4ca9cc985676f6c024441
Billah, Tashrif <tbillah@bwh.harvard.edu>
2022-04-22 15:56:17 -0400

    move all vars out of script

--- dpimport_remote_data.sh
+++ dpimport_remote_data.sh
@@ -1,19 +1,16 @@
 #!/usr/bin/env bash
 
-HOST=
-PORT=
-state=
-MONGO_PASS=
-CONFIG=
-
 export PATH=/data/predict/mongodb-linux-x86_64-rhel70-4.4.6/bin:$PATH
 
 
 if [ -z $HOST ] || [ -z $PORT ] || [ -z $state ] || [ -z $MONGO_PASS ] || [ -z $CONFIG ]
 then
-    echo Define state and MONGO_PASS and try again
+    echo Define HOST, PORT, state, MONGO_PASS, CONFIG and try again
     exit 1
 fi
+
+echo Importing to mongodb://dpdash:MONGO_PASS@$HOST:$PORT
+echo ''
 
 # delete old collections
 mongo --tls --tlsCAFile $state/ssl/ca/cacert.pem --tlsCertificateKeyFile $state/ssl/mongo_client.pem mongodb://dpdash:$MONGO_PASS@$HOST:$PORT/dpdata?authSource=admin /data/predict/utility/remove_studies.js
