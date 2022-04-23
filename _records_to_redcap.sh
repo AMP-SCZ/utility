34c10622c5be8f060b72bc55ddf5a0cca7399e69
Billah, Tashrif <tbillah@bwh.harvard.edu>
2022-04-23 14:47:20 -0400

    export records to redcap

---
+++ _records_to_redcap.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# this wrapper script is essential because #BSUB -J redcap-import[1-$N]
+# is not allowed within records_to_redcap.lsf
+
+# clear previous logs
+rm /data/predict/utility/bsub/*
+
+export redcap_phoenix=/data/predict/kcho/flow_test/Pronet/PHOENIX/PROTECTED
+export redcap_records=/data/predict/utility/bsub/redcap_records.txt
+# export is to allow them to be used within records_to_redcap.lsf
+
+cd $redcap_phoenix
+find . -name *Pronet.json > $redcap_records
+N=`cat $redcap_records | wc -l`
+
+bsub -J "redcap-import[1-$N]%12" < /data/predict/utility/records_to_redcap.lsf
+
