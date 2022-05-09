688da043c825fba9de94212f13f0e35df50a5f57
Billah, Tashrif <tbillah@bwh.harvard.edu>
2022-05-09 12:49:40 -0400

    check json for modification before uploading

--- import_records.py
+++ import_records.py
@@ -6,7 +6,10 @@ import sys
 import json
 from copy import deepcopy
 from tempfile import NamedTemporaryFile
-from os import remove
+from os import remove, stat
+from os.path import isfile, abspath, basename, dirname, join as pjoin
+from numpy import save, load
+from hashlib import md5
 
 
 if len(sys.argv)!=2 or sys.argv[1] in ['-h','--help']:
@@ -48,8 +51,37 @@ fields = {
     'returnFormat': 'json'
 }
 
-r = requests.post(config['api_url'], data=fields)
-print('HTTP Status: ' + str(r.status_code))
-print(r.json())
 
+# load and compare old os.stat() of REDCap JSON file
+hashfile= pjoin(abspath(dirname(__file__)), '.json_os.stat_hashes.npy')
+if isfile(hashfile):
+    hashes= load(hashfile, allow_pickle=True).item()
+else:
+    hashes= {}
 
+json_file= basename(sys.argv[1])
+if json_file in hashes:
+    old_hash= hashes[json_file]
+else:
+    old_hash= ''
+
+curr_stat= stat(sys.argv[1])
+curr_stat= '_'.join(str(s) for s in [curr_stat.st_uid,curr_stat.st_size,curr_stat.st_mtime])
+curr_hash= md5(curr_stat.encode('utf-8')).hexdigest()
+
+hashes1= deepcopy(hashes)
+hashes1[json_file]= curr_hash
+if curr_hash != old_hash:
+    
+    print(json_file, 'does not exist in REDCap or has been modified, uploading to REDCap')
+    
+    # upload to REDCap
+    r = requests.post(config['api_url'], data=fields)
+    print('HTTP Status: ' + str(r.status_code))
+    print(r.json())
+    
+    # save new hash
+    save(hashfile, hashes1)
+
+else:
+    print(json_file, 'has not been modified, skipping')
