4ed2baeb1276302e1f6a9249a40e942ac6d84556
Tashrif Billah <tbillah@bwh.harvard.edu>
2022-05-15 21:19:17 -0400

    generalize for any NDA_ROOT folder

--- generate_files_status.sh
+++ generate_files_status.sh
@@ -1,17 +1,18 @@
 #!/usr/bin/env bash
 
 export PATH=/data/predict/utility/:$PATH
+NDA_ROOT=$1
 
 rm /data/predict/kcho/flow_test/files_metadata.csv
 
 # use pnlpipe3 environment to bypass the error below
 # https://gist.github.com/tashrifbillah/24efeec3219ba3c58c92adc419aac7be#gistcomment-4037001
 source /data/pnl/soft/pnlpipe3/miniconda3/bin/activate && conda activate pnlpipe3 && \
-subject_files_status_for_dpdash.py && \
-cd /data/predict/kcho/flow_test/Pronet_status && \
+subject_files_status_for_dpdash.py $NDA_ROOT && \
+cd $NDA_ROOT/Pronet_status && \
 project_files_status_for_dpdash.py ProNET ../files_metadata.csv *-flowcheck-day1to1.csv && \
 chmod g+w * && \
-cd /data/predict/kcho/flow_test/Prescient_status && \
+cd $NDA_ROOT/Prescient_status && \
 project_files_status_for_dpdash.py PRESCIENT ../files_metadata.csv *-flowcheck-day1to1.csv && \
 chmod g+w *
 
