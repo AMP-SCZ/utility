c05be1f6ad4fc0e4fc0f185f6c383099e3d227f5
Billah, Tashrif <tbillah@bwh.harvard.edu>
2022-05-17 16:40:18 -0400

    update time

--- _crontab.sh
+++ _crontab.sh
@@ -3,8 +3,8 @@
 25 * * * * /data/predict/utility/generate_files_status.sh /data/predict/kcho/flow_test/ dpstage
 
 # import AVL QC data, daily at 4 pm
-0 16 * * * /data/predict/utility/dpimport_avlqc.sh /data/predict/data_from_nda/ rc-predict
-0 16 * * * /data/predict/utility/dpimport_avlqc.sh /data/predict/kcho/flow_test/ dpstage
+00 16 * * * /data/predict/utility/dpimport_avlqc.sh /data/predict/data_from_nda/ rc-predict
+05 16 * * * /data/predict/utility/dpimport_avlqc.sh /data/predict/kcho/flow_test/ dpstage
 
 # populate clone of site records, daily at 6 am
 0 6 * * * /data/predict/utility/_records_to_redcap.sh
