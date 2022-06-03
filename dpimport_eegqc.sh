5ec7e1981393e5772083d85feaad4ff3760e4afe
Billah, Tashrif <tbillah@bwh.harvard.edu>
2022-06-03 14:44:26 -0400

    dpimport_eegqc first pass

---
+++ dpimport_eegqc.sh
@@ -0,0 +1,65 @@
+#!/usr/bin/env bash
+
+export PATH=/data/predict/mongodb-linux-x86_64-rhel70-4.4.6/bin:$PATH
+
+if [ -z $1 ] || [ ! -d $1 ]
+then
+    echo """./dpimport_eegqc.sh /path/to/nda_root/ VM
+Provide /path/to/nda_root/ and VM
+VM name examples:
+    dpstage for dpstage.dipr.partners.org
+    rc-predict for rc-predict.bwh.harvard.edu
+    It is the first part of the server name."""
+    exit
+else
+    export NDA_ROOT=$1
+fi
+
+source /data/predict/utility/.vault/.env.${2}
+
+
+# remove old data
+mongo --tls --tlsCAFile $state/ssl/ca/cacert.pem \
+--tlsCertificateKeyFile $state/ssl/mongo_client.pem \
+mongodb://dpdash:$MONGO_PASS@$HOST:$PORT/dpdata?authSource=admin \
+--eval "studies=[\"EEGqc\"]" /data/predict/utility/avlqc_remove_study.js
+echo ''
+
+mongo --tls --tlsCAFile $state/ssl/ca/cacert.pem \
+--tlsCertificateKeyFile $state/ssl/mongo_client.pem \
+mongodb://dpdash:$MONGO_PASS@$HOST:$PORT/dpdata?authSource=admin \
+--eval "db.metadata.remove({\"study\":\"EEGqc\"})"
+echo ''
+
+
+# import new data
+source /data/pnl/soft/pnlpipe3/miniconda3/bin/activate base && conda activate dpimport
+
+pushd .
+cd $NDA_ROOT
+FEATURE_DIR=${NDA_ROOT}/EEGqc_features
+rm -f ${FEATURE_DIR}/*-day1to9999.csv
+out_template=${FEATURE_DIR}/AMPSCZ-SITE-EEGqc-day1to9999.csv
+
+cd /data/predict/kcho/flow_test/spero/Pronet
+/data/predict/utility/feature_combiner.py $out_template
+cd /data/predict/kcho/flow_test/spero/Prescient
+/data/predict/utility/feature_combiner.py $out_template
+
+for net in Pronet PRESCIENT
+do
+    echo Combining $net features
+    for d in `ls -d $net/PHOENIX/PROTECTED/${net}*`
+    do
+        echo Combining $d features
+        cd $d
+        /data/predict/utility/feature_combiner.py $out_template
+    done
+done
+popd
+
+exit
+cd $FEATURE_DIR
+import.py -c /data/predict/dpimport/examples/$CONFIG "*.csv"
+
+
