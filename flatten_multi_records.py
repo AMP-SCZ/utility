bd8e53083284e1f17edcc770b167e51ee0fb9feb
Billah, Tashrif <tbillah@bwh.harvard.edu>
2022-06-04 22:54:14 -0400

    generate dict and then make DataFrame

--- flatten_multi_records.py
+++ flatten_multi_records.py
@@ -1,4 +1,8 @@
+#!/usr/bin/env python
+
 import pandas as pd
+import sys
+import re
 
 multi_records= {'adverse_events': {'visit': 99, 'vars': ['chrae_?', 'chrae_tp?', 'chrae_diag?', 'chrae_ae?', 'chrae_aes?date', 'chrae_aes?off', 'chr_ae?date', 'chrae_sig?', 'chrae_dr?', 'chrae_d?', 'chrae_e?', 'chrae_expected?', 'chrae_sae?', 'chrae_ssi?', 'chr_ae?date_dr', 'chrae_sig?_dr', 'chrae_ae?_trans_q', 'chrae_ae?_mo?', 'chrae_ae?_mo2', 'chrae_ae?_mo3', 'chrae_ae?_mo4', 'chrae_ae?_mo5', 'chrae_ae?_mo6', 'chrae_ae?_mo7', 'chrae_ae?_mo8', 'chrae_ae?_mo9', 'chrae_ae?_mo10', 'chrae_ae?_mo11', 'chrae_ae?_mo12', 'chrae_ae?_mo18', 'chrae_ae?_mo24', 'chrae_ae?_trans', 'chrae_ae?_offmes', 'chrae_ae?_comments']},
 'psychosocial_treatment_form': {'visit': 99, 'vars': ['chrpsychsoc_treat?', 'chrpsychsoc_treat?_tp', 'chrpsychsoc_treat?_type', 'chrpsychsoc_treat?_other', 'chrpsychsoc_treat?_res', 'chrpsychsoc_treat?_freq', 'chrpsychsoc_treat?_onset', 'chrpsychsoc_treat?_offset', 'chrpsychsoc_treat?_instruct', 'chrpsychsoc_treat?_instruct_other', 'chrpsychsoc_treat?_trans_q', 'chrpsychsoc_treat?_mo1', 'chrpsychsoc_treat?_mo2', 'chrpsychsoc_treat?_mo3', 'chrpsychsoc_treat?_mo4', 'chrpsychsoc_treat?_mo5', 'chrpsychsoc_treat?_mo6', 'chrpsychsoc_treat?_mo7', 'chrpsychsoc_treat?_mo8', 'chrpsychsoc_treat?_mo9', 'chrpsychsoc_treat?_mo10', 'chrpsychsoc_treat?_mo11', 'chrpsychsoc_treat?_mo12', 'chrpsychsoc_treat?_mo18', 'chrpsychsoc_treat?_mo24', 'chrpsychsoc_treat?_trans', 'chrpsychsoc_treat?_offmes', 'chrpsychsoc_treat?_comments']},
@@ -14,8 +18,13 @@ form= re.search(f'{subjectkey}_(.+?).csv', sys.argv[1]).group(1)
 df= pd.read_csv(sys.argv[1])
 cols= df.columns[5:-1]
 
+# TBD copy non-repeat variables, do it in a different for loop
+
 # new flat data frame with default columns
 df1= df[df.columns[:5]]
+
+dict1={}
+dict1['visit']= multi_records[form]['visit']
 for v in multi_records[form]['vars']:
 
     # has the variable been exported by RPMS?
@@ -28,9 +37,11 @@ for v in multi_records[form]['vars']:
         
     # go through the rows of that variable and generate a flat list
     for i,row in df.iterrows():
-        df1.at[i, v.replace('?',str(row['Row#']))]= row[c]
+        dict1[v.replace('?',str(row['Row#']))]= row[c]
         
 
-df.to_csv(sys.argv[1]+'.flat', index=False)
+df1= pd.concat([df1,pd.DataFrame([dict1])], axis=1)
+
+df1.to_csv(sys.argv[1]+'.flat', index=False)
 
 
