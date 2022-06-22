7f517484ba94429c21ebed4920ec530efe949988
Billah, Tashrif <tbillah@bwh.harvard.edu>
2022-06-22 16:07:59 -0400

    remove metadata children collections too

--- remove_studies.js
+++ remove_studies.js
@@ -3,18 +3,22 @@
 // all sites and pseudo sites: PRoNET, PRESCIENT, files
 let sites= ['ME', 'AD', 'CG', 'JE', 'CP', 'BM', 'AM', 'LS', 'GW', 'SG', 'HK', 'LA', 'OR', 'BI', 'NL', 'NC', 'SD', 'CA', 'YA', 'SF', 'PA', 'SI', 'PI', 'NN', 'IR', 'TE', 'GA', 'WU', 'HA', 'MT', 'KC', 'PV', 'MA', 'CM', 'MU', 'SH', 'SL'].concat(["ProNET", "PRESCIENT", "files"]);
 
-sites.forEach(s => {
+['toc','metadata'].forEach(g=> {
+    print('Removing',g)
 
-    print(s);
+    sites.forEach(s => {
 
-    // delete children collections
-    let colls= db.toc.find({"study":s});
-    let num= colls.length();
+        print(s);
 
-    for (let i=0; i<num; i++) {
-            print(db[ colls[i].collection ].remove({}));
-    }
+        // delete children collections
+        let colls= db[g].find({"study":s});
+        let num= colls.length();
 
-    // delete parent collection
-    print(db.toc.remove({"study":s}));
+        for (let i=0; i<num; i++) {
+                print(db[ colls[i].collection ].remove({}));
+        }
+
+        // delete parent collection
+        print(db[g].remove({"study":s}));
+    })
 })
