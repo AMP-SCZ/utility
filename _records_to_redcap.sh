41ecfe7db973258491b0dbffc61d8ef9a1a3e475
Billah, Tashrif <tbillah@bwh.harvard.edu>
2022-06-22 17:52:12 -0400

    all vars defined via argv

--- _records_to_redcap.sh
+++ _records_to_redcap.sh
@@ -3,11 +3,12 @@
 # this wrapper script is essential because #BSUB -J redcap-import[1-$N]
 # is not allowed within records_to_redcap.lsf
 
-# clear previous logs
-rm /data/predict/utility/bsub/*
-
-export redcap_phoenix=/data/predict/kcho/flow_test/Pronet/PHOENIX/PROTECTED
-export redcap_records=/data/predict/utility/bsub/redcap_records.txt
+redcap_records=/data/predict/utility/bsub/redcap_records.txt
+redcap_phoenix=$1
+redcap_dict=$2
+API_TOKEN=$3
+FORCE=1
+export redcap_records redcap_phoenix redcap_dict API_TOKEN FORCE
 # export is to allow them to be used within records_to_redcap.lsf
 
 cd $redcap_phoenix
@@ -16,6 +17,6 @@ N=`cat $redcap_records | wc -l`
 
 source /etc/profile
 # prevent getting thousand emails
-# export LSB_JOB_REPORT_MAIL=N
+export LSB_JOB_REPORT_MAIL=N
 bsub -J "redcap-import[1-$N]%12" < /data/predict/utility/records_to_redcap.lsf
 
