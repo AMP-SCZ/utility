1708de93254a9e479ce41bc0d39fb452f6abbd3f
Billah, Tashrif <tbillah@bwh.harvard.edu>
2022-06-23 16:53:38 -0400

    update list of crons

--- _crontab.sh
+++ _crontab.sh
@@ -1,11 +1,20 @@
 # generate lochness datalake's files status hourly for dpdash, hourly
 20 * * * * /data/predict/utility/generate_files_status.sh /data/predict/data_from_nda/ rc-predict
-25 * * * * /data/predict/utility/generate_files_status.sh /data/predict/kcho/flow_test/ dpstage
+25 * * * * /data/predict/utility/generate_files_status.sh /data/predict/data_from_nda_dev/ dpstage
 
 # import AVL QC data, daily at 4 pm
 00 16 * * * /data/predict/utility/dpimport_avlqc.sh /data/predict/data_from_nda/ rc-predict
-05 16 * * * /data/predict/utility/dpimport_avlqc.sh /data/predict/kcho/flow_test/ dpstage
+05 16 * * * /data/predict/utility/dpimport_avlqc.sh /data/predict/data_from_nda_dev/ dpstage
 
-# populate clone of site records, daily at 6 am
-0 6 * * * /data/predict/utility/_records_to_redcap.sh
+# clear REDCap upload logs, every monday at 12 am
+0 * * * 1 rm -f /data/predict/utility/bsub/*
 
+# import records to REDCap, every third hour
+# PRESCIENT mock
+0 */3 * * * /data/predict/utility/records_to_redcap.sh /data/predict/data_from_nda_dev/Prescient/PHOENIX/PROTECTED /data/predict/utility/amp-scz-form 123456
+
+# ProNET mock
+20 */3 * * * /data/predict/utility/records_to_redcap.sh /data/predict/data_from_nda_dev/Pronet/PHOENIX/PROTECTED /data/predict/utility/redcap-ii-yale 123456
+
+# ProNET real
+40 */3 * * * /data/predict/utility/records_to_redcap.sh /data/predict/data_from_nda/Pronet/PHOENIX/PROTECTED /data/predict/utility/yale-real 123456
