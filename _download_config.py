86539ef3307d5a848f395fadcb6d7d50f080bf38
Billah, Tashrif <tbillah@bwh.harvard.edu>
2022-06-27 11:52:22 -0400

    omit _copy from imported config name

--- _download_config.py
+++ _download_config.py
@@ -6,7 +6,7 @@ with open(sys.argv[1]) as f:
 
 dict2={}
 dict2['config']=dict1['config']['0']
-dict2['name']=dict1['name']+'_copy'
+# dict2['name']=dict1['name']+'_copy'
 
 with open(sys.argv[2], 'w') as f:
     json.dump(dict2,f,indent=3)
