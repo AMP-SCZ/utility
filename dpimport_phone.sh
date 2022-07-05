d03e4c58d6ccb5e4ed1b01428393280e86185b24
Billah, Tashrif <tbillah@bwh.harvard.edu>
2022-07-05 15:48:10 -0400

    also remove actigraphy data

--- dpimport_phone.sh
+++ dpimport_phone.sh
@@ -25,6 +25,12 @@ mongodb://dpdash:$MONGO_PASS@$HOST:$PORT/dpdata?authSource=admin \
 --eval "assess=\"-phone_\"" /data/predict/utility/remove_assess.js
 echo ''
 
+mongo --tls --tlsCAFile $state/ssl/ca/cacert.pem \
+--tlsCertificateKeyFile $state/ssl/mongo_client.pem \
+mongodb://dpdash:$MONGO_PASS@$HOST:$PORT/dpdata?authSource=admin \
+--eval "assess=\"-actigraphy_\"" /data/predict/utility/remove_assess.js
+
+
 # import new data
 export PATH=/data/predict/miniconda3/bin:$PATH
 cd ${NDA_ROOT}
