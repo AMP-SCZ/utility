ee5b9d509dd7d7ac371dd7ad72034bd2254a72d7
kc244 <kevincho@bwh.harvard.edu>
2022-08-24 15:10:28 -0400

    feat: ignore shim settings in mri qc result check

--- subject_files_status_for_dpdash.py
+++ subject_files_status_for_dpdash.py
@@ -119,14 +119,13 @@ def check_mri_and_qqc_result(root: Path) -> int:
 
     # load QC result
     session_name = 'ses-' + ''.join(name_sections[2:]).split('.')[0]
-    qqc_dir = mri_qqc_root / ('sub-'+root.name) /session_name
+    qqc_dir = mri_qqc_root / f'sub-{root.name}' / session_name
     qc_summary = qqc_dir / '00_qc_summary.csv'
 
     labels_to_check = ['Series count',
                        'Volume slice number comparison',
                        'Image orientation in anat',
                        'Image orientation in others',
-                       'Shim settings',
                        'Bval comparison']
 
     if qc_summary.is_file():
