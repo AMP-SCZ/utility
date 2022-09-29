d421aa5862c7afb9ab0d949c8ae500b6f15bd380
Billah, Tashrif <tbillah@bwh.harvard.edu>
2022-09-29 17:55:09 -0400

    wip dict diff gen and emailing script

---
+++ _gen_dict_diff.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+
+cd /data/predict/utility/dict_diff
+CURL=`which curl`
+datestamp=$(date +"%Y%m%d")
+
+# download ground truth dictionary
+ampscz=ampscz/ampscz_dict_${datestamp}.csv
+DATA="token=${1}&content=metadata&format=csv&returnFormat=json"
+$CURL -H "Content-Type: application/x-www-form-urlencoded" \
+      -H "Accept: application/json" \
+      -X POST \
+      -d $DATA \
+      https://redcap.partners.org/redcap/api/ > $ampscz
+
+
+# download pronet dictionary
+pronet=pronet/pronet_dict_${datestamp}.csv
+DATA="token=${2}&content=metadata&format=csv&returnFormat=json"
+$CURL -H "Content-Type: application/x-www-form-urlencoded" \
+      -H "Accept: application/json" \
+      -X POST \
+      -d $DATA \
+      https://redcap.partners.org/redcap/api/ > $pronet
+
+
+# download prescient dictionary
+prescient=prescient/prescient_dict_${datestamp}.csv
+DATA="token=${3}&content=metadata&format=csv&returnFormat=json"
+$CURL -H "Content-Type: application/x-www-form-urlencoded" \
+      -H "Accept: application/json" \
+      -X POST \
+      -d $DATA \
+      https://redcap.partners.org/redcap/api/ > $prescient
+
+
+
+cd ..
+for net in pronet prescient
+do
+    ./gen_dict_diff.py $ampscz ${net}/${net}_dictionary_${datestamp}.csv $net
+    suffix=${net}_${datestamp}
+
+    # email report
+    for p in ${@: 4:$#}
+    do
+
+       echo "" | mailx -s diff_ampscz_${suffix} \
+       -a ampscz_vars_absent_in_${suffix}.csv \
+       -a branch_logic_diff_ampscz_${suffix}.csv \
+       -a calc_diff_ampscz_${suffix}.csv \
+       -- ${p}.partners.org
+done
+
+
