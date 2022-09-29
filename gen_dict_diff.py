134c4802cc1d33492b549ef6b443c31b599d6060
Billah, Tashrif <tbillah@bwh.harvard.edu>
2022-09-29 16:49:15 -0400

    generate dict diff, wip

---
+++ gen_dict_diff.py
@@ -0,0 +1,73 @@
+#!/usr/bin/env python
+
+from os.path import abspath, dirname, join as pjoin
+from os import getcwd, chdir
+SCIRPTDIR=dirname(abspath(__file__))
+
+# ground truth dictionary
+df1=pd.read_csv('')
+# network's dictionary
+df2=pd.read_csv('')
+
+network='yale'
+datestamp=datetime.now().strftime('%Y%m%d')
+suffix=f'{network}_{datestamp}'
+
+var_header='Variable / Field Name'
+form_header='Form Name'
+branch_header='Branching Logic (Show field only if...)'
+calc_header='Choices, Calculations, OR Slider Labels'
+
+ground_groups=df1.groupby(form_header)
+target_groups=df2.groupby(form_header)
+
+
+nonexistent_vars=[]
+df_branch=pd.DataFrame(columns=[var_header,'AMP-SCZ logic','Network logic'])
+df_branch.set_index(var_header, inplace=True)
+df_calc=pd.DataFrame(columns=[var_header,'AMP-SCZ calculation','Network calculation'])
+df_calc.set_index(var_header, inplace=True)
+
+for form in ground_groups.groups.keys():
+    if form not in target_groups.groups.keys():
+        continue
+        
+    if '_consent_' in form and 'informed_consent_run_sheet' not in form:
+        continue
+    
+    print(form)
+    
+    dfg= ground_groups.get_group(form)    
+    dft= target_groups.get_group(form)
+    
+    dfg.set_index(var_header, inplace=True)
+    dft.set_index(var_header, inplace=True)
+    
+    for v,row in dfg.iterrows():
+        try:
+            dft.loc[v]
+        except KeyError:
+            nonexistent_vars.append(v)
+            continue
+            
+        
+        # compare branching logic
+        if not pd.isna(row[branch_header]) and row[branch_header]!=dft.loc[v,branch_header]:
+            df_branch.loc[v]= [row[branch_header], dft.loc[v,branch_header]]
+        
+        # compare calculation
+        if not pd.isna(row[calc_header]) and row[calc_header]!=dft.loc[v,calc_header]:
+            df_calc.loc[v]= [row[calc_header], dft.loc[v,calc_header]]
+    
+
+
+df_var=pd.DataFrame(data={var_header:nonexistent_vars})
+
+dir_bak=getcwd()
+chdir(pjoin(SCRIPTDIR,'dict_diff'))
+df_var.to_csv(f'ampscz_vars_absent_in_{suffix}.csv', index=False)
+df_branch.to_csv(f'branching_logic_diff_between_ampscz_{suffix}.csv')
+df_calc.to_csv(f'calculation_diff_between_ampscz_{suffix}.csv')
+chdir(dir_bak)
+
+
