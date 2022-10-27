04475df32f0652debb746dda05a32f13359554f0
Billah, Tashrif <tbillah@bwh.harvard.edu>
2022-10-27 18:00:14 -0400

    require config_owner to download config

--- download_config.sh
+++ download_config.sh
@@ -2,9 +2,9 @@
 
 export PATH=/data/predict/mongodb-database-tools-rhel70-x86_64-100.5.2/bin:$PATH
 
-if [[ $# -lt 2 ]]
+if [[ $# -lt 3 ]]
 then
-    echo "Usage: ./download_config.sh config_name /tmp/config_name.json"
+    echo "Usage: ./download_config.sh config_name owner_id /tmp/config_name.json"
     echo "Provide configuration name and output file name"
     exit 1
 fi
@@ -24,10 +24,10 @@ tmpJson=/tmp/config_`pgrep -f $0`.json
 scriptDir=`dirname $0`
 
 # export from mongodb
-mongoexport --ssl --sslCAFile=$state/ssl/ca/cacert.pem --sslPEMKeyFile=$state/ssl/mongo_client.pem --uri="mongodb://dpdash:$MONGO_PASS@$HOST:$PORT/dpdmongo?authSource=admin" --collection=configs --query="{\"name\":\"$1\"}" --out=$tmpJson
+mongoexport --ssl --sslCAFile=$state/ssl/ca/cacert.pem --sslPEMKeyFile=$state/ssl/mongo_client.pem --uri="mongodb://dpdash:$MONGO_PASS@$HOST:$PORT/dpdmongo?authSource=admin" --collection=configs --query="{\"name\":\"$1\",\"owner\":\"$2\"}" --out=$tmpJson
 # modify according to https://github.com/AMP-SCZ/dpdash/wiki/Configuration-schema
-source /data/predict/miniconda3/bin/activate base
-$scriptDir/_download_config.py $tmpJson $2
+export PATH=/data/predict/miniconda3/bin:$PATH
+$scriptDir/_download_config.py $tmpJson $3
 
 rm $tmpJson
 
