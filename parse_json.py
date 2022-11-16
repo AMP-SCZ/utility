391f0c832667d4b8868d68fd40915ad23ebb0b0e
Tashrif Billah <tbillah@bwh.harvard.edu>
2022-11-16 14:40:44 -0500

    parse REDCap jsons by event and variable names

---
+++ parse_json.py
@@ -0,0 +1,71 @@
+#!/usr/bin/env python
+
+from os import environ, getcwd, chdir
+environ['OPENBLAS_NUM_THREADS']='16'
+
+from glob import glob
+import json,sys
+import pandas as pd
+from os.path import abspath
+import yaml
+
+if len(sys.argv)<3 or sys.argv[1]=='-h' or sys.argv[1]=='--help':
+    print(f"""A command line program for making REDCap-like reports.
+Usage:
+./{__file__} /path/to/event_var.yaml /path/to/Network/PHOENIX/PROTECTED
+./{__file__} /path/to/event_var.yaml /path/to/Network/PHOENIX/PROTECTED */raw/*/surveys/*.Pronet.json
+The last template is optional.
+Sample yaml file:
+
+screening_arm_1:
+    - chrguid_guid
+    - chrhealth_alle
+
+baseline_arm_1:
+    - chrdemo_age_mos_chr
+
+baseline_arm_2:
+    - chrdemo_age_mos_hc
+
+""")
+
+
+with open(sys.argv[1]) as f:
+    rows=yaml.safe_load(f)
+
+dir_bak=getcwd()
+chdir(sys.argv[2])
+
+template='*/raw/*/surveys/*.Pronet.json'
+if len(sys.argv)==4:
+    template=sys.argv[3]
+
+files=glob(template)
+
+L=len(files)
+for event,vars in rows.items():
+    values[event]=''
+    for v in vars:
+        values[event][v]=['']*L
+
+for i,file in enumerate(files):
+    with open(file) as f:
+        dict1=json.load(f)
+
+
+    for event,vars in rows.items():
+
+        for v in vars:
+
+            print(event,v)
+
+            for d in dict1:
+                if d['redcap_event_name']==event:
+                    values[event][v][i]=d['chric_record_id']+' '+d[v]
+                    break
+
+            print(values[event][v])
+            print('===================\n')
+
+
+chdir(dir_bak)
