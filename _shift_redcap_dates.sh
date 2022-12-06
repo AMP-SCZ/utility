b4dfb1c0ae64b131a84b0427fcec5863d1a0a321
Billah, Tashrif <tbillah@bwh.harvard.edu>
2022-12-06 17:15:29 -0500

    wrapper for setting shift and shifting REDCap JSONs

---
+++ _shift_redcap_dates.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+export OPENBLAS_NUM_THREADS=16
+
+export PATH=/data/predict/miniconda3/bin:$PATH
+
+PHOENIX_PROTECTED=$1
+JSON=$2
+
+cd /data/predict/utility/
+./_set_date_shifts.py $PHOENIX_PROTECTED $JSON
+./shift_redcap_dates.py $PHOENIX_PROTECTED $JSON $3
+
+
