c9d3d5358bddc5f7ffd943d5e9b209f781123f94
Billah, Tashrif <tbillah@bwh.harvard.edu>
2022-12-06 21:22:40 -0500

    include date shifter in crontab archive

--- _crontab.sh
+++ _crontab.sh
@@ -9,12 +9,20 @@
 # clear REDCap upload logs, every monday at 12 am
 0 * * * 1 rm -f /data/predict/utility/bsub/*
 
-# import records to REDCap, every third hour
+# import records to REDCap, once a week
+# used to be 0 */3, 15 */3, 30 */3, 45 */3 (every three hours)
+# changed to only once a week to unburden the compute nodes
 # PRESCIENT mock
-0 */3 * * * /data/predict/utility/_rpms_to_redcap.sh /data/predict/data_from_nda_dev/Prescient/PHOENIX/PROTECTED /data/predict/utility/amp-scz-form 123456
+0 0 * * 0 /data/predict/utility/_rpms_to_redcap.sh /data/predict/data_from_nda_dev/Prescient/PHOENIX/PROTECTED /data/predict/utility/amp-scz-form 123456
+
+# PRESCIENT real
+0 0 * * 1 /data/predict/utility/_rpms_to_redcap.sh /data/predict/data_from_nda/Prescient/PHOENIX/PROTECTED /data/predict/utility/amp-scz-form 123456
 
 # ProNET mock
-20 */3 * * * /data/predict/utility/_records_to_redcap.sh /data/predict/data_from_nda_dev/Pronet/PHOENIX/PROTECTED /data/predict/utility/redcap-ii-yale 123456
+0 0 * * 2 /data/predict/utility/_records_to_redcap.sh /data/predict/data_from_nda_dev/Pronet/PHOENIX/PROTECTED /data/predict/utility/redcap-ii-yale 123456
 
 # ProNET real
-40 */3 * * * /data/predict/utility/_records_to_redcap.sh /data/predict/data_from_nda/Pronet/PHOENIX/PROTECTED /data/predict/utility/yale-real 123456
+0 23 * * * /data/predict/utility/_shift_redcap_dates.sh /data/predict/data_from_nda/Pronet/PHOENIX/PROTECTED "*/raw/*/surveys/*.Pronet.json" /data/predict/utility/yale-real/ProNETPsychosisRiskOutcomesNet_DataDictionary_2022-10-28_checkbox.csv
+0 0 * * 3 /data/predict/utility/_records_to_redcap.sh /data/predict/data_from_nda/Pronet/PHOENIX/PROTECTED /data/predict/utility/yale-real 123456
+
+
