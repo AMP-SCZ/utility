8041a337b43519bb8cebe2c596aa6db4964055d8
Billah,Tashrif <tbillah@bwh.harvard.edu>
2022-12-14 09:28:40 +1100

    * add missing imports * move copyfile block into for loop * some typo

--- rename_RPMS_vars.py
+++ rename_RPMS_vars.py
@@ -4,26 +4,33 @@ from shutil import copyfile
 from yaml import safe_load
 from glob import glob
 from datetime import date
+from os import getcwd, chdir
+import sys
 
 suffix=date.today().strftime('%d.%m.%Y.csv')
-
+suffix='13.12.2022.csv'
 with open('rename_RPMS_vars.yaml') as f:
     dict1=safe_load(f)
+
+dir_bak=getcwd()
+chdir(sys.argv[1])
     
-for prefix in dict1.keys()
+for prefix in dict1.keys():
     file=prefix+suffix
     
     with open(file) as f:
         content=f.read()
         
     
-    for line in dict1[k]:
-        v,vnew=line.split()
+    for line in dict1[prefix]:
+        v,vnew=line.split(',')
         
         content=content.replace(v,vnew)
         
 
-copyfile(file, file+'.bak')
-with open(file,'w') as f:
-    f.write(content)
+    copyfile(file, file+'.bak')
+    with open(file,'w') as f:
+        f.write(content)
+
+chdir(dir_bak)
 
