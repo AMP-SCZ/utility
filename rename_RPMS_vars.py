c738441f66c8425996da7c2c5461279ea00368fe
Billah, Tashrif <tbillah@bwh.harvard.edu>
2022-12-14 17:37:03 -0500

    * typo * skip RPMS files at DPACC and vice versa

--- rename_RPMS_vars.py
+++ rename_RPMS_vars.py
@@ -5,11 +5,11 @@ from yaml import safe_load
 from glob import glob
 from datetime import date
 from os import getcwd, chdir, environ
+from os.path import isfile
 import sys
 import re
 
-if environ['HOSTNAME'].endswith('orygen.org.au'):
-    suffix=date.today().strftime('%d.%m.%Y.csv')
+suffix=date.today().strftime('%d.%m.%Y.csv')
 
 with open('rename_RPMS_vars.yaml') as f:
     dict1=safe_load(f)
@@ -20,13 +20,22 @@ chdir(sys.argv[1])
 for pattern in dict1.keys():
 
     if environ['HOSTNAME'].endswith('orygen.org.au'):
+        # rename vars at RPMS end
         file=pattern+suffix
-    else:
-        try:
-            file=glob('*'+pattern)[0]
-        except IndexError:
-            # file does not exist
+        if not isfile:
+            # this file exists at DPACC
+            # skip DPACC suffixes
             continue
+
+    else:
+        # rename vars at DPACC end
+        file=glob('*'+pattern)
+        if not file:
+            # this file does not exist for this subject
+            # skip RPMS prefixes
+            continue
+
+        file=file[0]
 
     
     with open(file) as f:
@@ -34,7 +43,7 @@ for pattern in dict1.keys():
         header=content.split('\n')[0]
         
     
-    for line in dict1[prefix]:
+    for line in dict1[pattern]:
         v,vnew=line.split(',')
         
         if vnew not in header:
