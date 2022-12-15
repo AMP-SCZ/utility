19268d526dd0fa4c2ab46b456c6d21263b231c42
Billah, Tashrif <tbillah@bwh.harvard.edu>
2022-12-15 11:46:39 -0500

    import combined phone viz

--- dpimport_phone.sh
+++ dpimport_phone.sh
@@ -36,5 +36,6 @@ export PATH=/data/predict/miniconda3/bin:$PATH
 cd ${NDA_ROOT}
 import.py -c /data/predict/dpimport/examples/$CONFIG "*/PHOENIX/PROTECTED/*/processed/*/phone/*/??-*.csv"
 import.py -c /data/predict/dpimport/examples/$CONFIG "*/PHOENIX/PROTECTED/*/processed/*/actigraphy/*/??-*.csv"
-
+import.py -c /data/predict/dpimport/examples/$CONFIG "digitalqc/??-*.csv"
+import.py -c /data/predict/dpimport/examples/$CONFIG "digitalqc/combined-*.csv"
 
