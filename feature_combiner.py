26b23dd44b2f8d2f1a12f9a12d1c4b25bf974871
Tashrif Billah <tbillah@bwh.harvard.edu>
2022-12-20 09:42:50 -0500

    * provide glob pattern from outside
    * df.fillna() for default cols
    * restore int data type

--- feature_combiner.py
+++ feature_combiner.py
@@ -7,21 +7,26 @@ from os.path import abspath
 import sys
 
 if len(sys.argv)<2 or sys.argv[1] in ['-h','--help']:
-    print(f'''Usage: {__file__} /path/to/AMPSCZ-SITE-assessment-day1to9999.csv
+    print(f'''Usage:
+{__file__} /path/to/combined-SITE-assessment-day1to1.csv "*/processed/*/eeg/??-*-EEGqc-day1to*.csv"
+{__file__} /path/to/combined-SITE-assessment-day1to1.csv "./**/??-*-EEGqc-day1to*.csv"
+
 This program combines CSV features at network or site level.
 Execute it from any of the example directories:
 $NDA_ROOT/Pronet
 $NDA_ROOT/Prescient
 $NDA_ROOT/Pronet/PHOENIX/PROTECTED/PronetCA
-$NDA_ROOT/Prescient/PHOENIX/PROTECTED/PrescientME''')
+$NDA_ROOT/Prescient/PHOENIX/PROTECTED/PrescientME
+with a proper glob() pattern''')
     exit(0)
 
 
-files= glob('./**/*-EEGqc-day1to*.csv',recursive=True)
+files= glob(sys.argv[2],recursive=True)
 if not files:
     print('No DPdash compatible CSV files to combine, exiting')
     exit()
 df= pd.read_csv(files[0])
+
 
 df1= pd.DataFrame(columns= list(df.columns)+['subject'])
 
@@ -35,6 +40,29 @@ for f in files:
         df1.loc[i]= list(row)+ [subject]
         df1.at[i,'day']= i+1
         i+=1
+
+
+# restore the data types
+dtype= {}
+
+# replace the NaNs for the purpose of dtype restoration
+# otherwise, we shall get
+# pandas.errors.IntCastingNaNError: Cannot convert non-finite values (NA or inf) to integer
+# even though we omit 'reftime','timeofday','weekday' from dtype
+df1.fillna(0, inplace=True)
+for c,d in zip(df.columns.values,df.dtypes):
+
+    if 'int' in d.name:
+        dtype[c]= 'short'
+    elif 'float' in d.name:
+        dtype[c]= 'float32'
+    else:
+        dtype[c]= d.name
+
+df1= df1.astype(dtype)
+
+# now reset the mandatory columns
+df1[['reftime', 'timeofday', 'weekday']]=''
 
 curr_dir= getcwd()
 if curr_dir.endswith('Pronet'):
