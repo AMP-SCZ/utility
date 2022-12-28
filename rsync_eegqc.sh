87605fb2c1003911b19657a30f69e441e73dd8e1
Billah, Tashrif <tbillah@bwh.harvard.edu>
2022-12-28 18:07:48 -0500

    define remote dest via argument too

--- rsync_eegqc.sh
+++ rsync_eegqc.sh
@@ -3,6 +3,6 @@
 NDA_ROOT=$1
 cd $NDA_ROOT
 rsync -avR */PHOENIX/PROTECTED/*/processed/*/eeg/*/Figures/*[!QC].png \
--avR */PHOENIX/PROTECTED/*/processed/*/eeg/*/Figures/WU01590_20220921_runSheet.* \
-rc-predict-gen.partners.org:/data/EEGqc/
+-avR */PHOENIX/PROTECTED/*/processed/*/eeg/*/Figures/*_runSheet.* \
+rc-predict-gen.partners.org:$2
 
