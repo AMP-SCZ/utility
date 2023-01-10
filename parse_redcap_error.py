b44210c098cfcc8249d1b518db49812e346d0e86
Billah, Tashrif <tbillah@bwh.harvard.edu>
2023-01-10 15:34:09 -0500

    included glob and loop within script

--- parse_redcap_error.py
+++ parse_redcap_error.py
@@ -10,7 +10,7 @@ for file in files:
     with open(file) as f:
         content=f.read().split('\n')
 
-    print('\033[92m','Parsing', file,'\033[0m')
+    print('\033[92m','Parsing',file,'\033[0m')
 
     for i,line in enumerate(content):
         if 'HTTP Status: 400' in line:
