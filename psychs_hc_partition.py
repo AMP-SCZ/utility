f8334044ad84f6402c17f9290170de3a0b1d25f1
Billah, Tashrif <tbillah@bwh.harvard.edu>
2023-01-17 18:31:24 -0500

    accept input dir instead of input file

--- psychs_hc_partition.py
+++ psychs_hc_partition.py
@@ -3,43 +3,52 @@
 import pandas as pd
 import sys
 from os.path import abspath, dirname
+from os import getcwd, chdir
+from glob import glob
 
 sys.path.append(dirname(abspath(__file__)))
 
 from idvalidator import validate
 
-# TBD revision: accept input dir and execute for both p1p8 and p9ac32
 
-dfpsychs=pd.read_csv(sys.argv[1])
-dfhc=pd.DataFrame(columns=[c.replace('chrpsychs','hcpsychs') for c in dfpsychs.columns])
+dir_bak=getcwd()
+chdir(sys.argv[1])
 
-dfpsychs.set_index('subjectkey',inplace=True)
-dfhc.set_index('subjectkey',inplace=True)
+files=[glob(p)[0] for p in ['PrescientStudy_Prescient_psychs_p1p8_fu_*.csv','PrescientStudy_Prescient_psychs_p9ac32_fu_*.csv']]
 
-dfincl=pd.read_csv(sys.argv[2])
+for file in files:
 
-j=0
-for i,row in dfincl.iterrows():
-    
-    if not validate(row['subjectkey']):
-        continue
-       
-    if row['chrcrit_part']==1:
-        # CHR
-        continue
-    
-    try:
-        dfhc.loc[row['subjectkey']]=dfpsychs.loc[row['subjectkey']].values
-    except KeyError:
-        continue
-    
-    j+=1
-  
-# TBD dtype conversion
-# TBD add _fu_hc.csv suffixes in EntryStatus form or try/except logic in rpms_to_redcap.py
+    dfpsychs=pd.read_csv(file)
+    dfhc=pd.DataFrame(columns=[c.replace('chrpsychs','hcpsychs') for c in dfpsychs.columns])
 
-dfhc.reset_index(inplace=True)
-outfile=sys.argv[1].replace('_fu_','_fu_hc_')
-dfhc.to_csv(outfile,index=False)
+    dfpsychs.set_index('subjectkey',inplace=True)
+    dfhc.set_index('subjectkey',inplace=True)
 
+    dfincl=pd.read_csv(sys.argv[2])
+
+    j=0
+    for i,row in dfincl.iterrows():
+        
+        if not validate(row['subjectkey']):
+            continue
+           
+        if row['chrcrit_part']==1:
+            # CHR
+            continue
+        
+        try:
+            dfhc.loc[row['subjectkey']]=dfpsychs.loc[row['subjectkey']].values
+        except KeyError:
+            continue
+        
+        j+=1
+      
+    # TBD dtype conversion
+
+    dfhc.reset_index(inplace=True)
+    outfile=sys.argv[1].replace('_fu_','_fu_hc_')
+    dfhc.to_csv(outfile,index=False)
+
+
+chdir(dir_bak)
 
