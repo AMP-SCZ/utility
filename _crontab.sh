05f8aed9b10fff51eb68f8d0c64e33f68d806e6f
Billah,Tashrif <tbillah@bwh.harvard.edu>
2023-01-21 01:25:27 +1100

    add rpms_psychs_partition.py to cron

--- _crontab.sh
+++ _crontab.sh
@@ -80,7 +80,8 @@ SHELL=/bin/bash
 HOSTNAME=1200941-Prescient.orygen.org.au
 
 MAILTO=xyz@bwh.harvard.edu
-50 20 * * * /usr/bin/rm /mnt/prescient/one_day_backup/* && /usr/bin/cp /mnt/prescient/RPMS_incoming/*csv /mnt/prescient/one_day_backup/
-0 21 * * * /home/tashrifbillah/miniconda3/bin/python /mnt/prescient/utility/rename_RPMS_vars.py /mnt/prescient/RPMS_incoming/
-30 21 * * * /home/tashrifbillah/miniconda3/bin/python /mnt/prescient/utility/replace_RPMS_values.py /mnt/prescient/RPMS_incoming/
+04 20 * * * /usr/bin/rm /mnt/prescient/one_day_backup/* && /usr/bin/cp /mnt/prescient/RPMS_incoming/*csv /mnt/prescient/one_day_backup/
+05 20 * * * /home/tashrifbillah/miniconda3/bin/python /mnt/prescient/utility/rename_RPMS_vars.py /mnt/prescient/RPMS_incoming/
+07 20 * * * /home/tashrifbillah/miniconda3/bin/python /mnt/prescient/utility/replace_RPMS_values.py /mnt/prescient/RPMS_incoming/
+10 20 * * * /mnt/prescient/utility/rpms_psychs_partition.py /mnt/prescient/RPMS_incoming/
 
