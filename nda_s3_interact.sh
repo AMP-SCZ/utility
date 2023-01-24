8f57cdc4bef1e7a38d6c75b1e60decac87d47566
Billah, Tashrif <35086881+tashrifbillah@users.noreply.github.com>
2023-01-24 18:38:45 -0500

    check sourced AWS_ACCESS_KEY_ID, fix indentation

--- nda_s3_interact.sh
+++ nda_s3_interact.sh
@@ -1,14 +1,22 @@
 #!/bin/bash
 
-    if [ $# -lt 2 ] || [ "$1" == "-h" ] || [ "$1" == "--help" ]
-    then
-        echo """Usage:
+if [ $# -lt 2 ] || [ "$1" == "-h" ] || [ "$1" == "--help" ]
+then
+    echo """Usage:
 /path/to/utility/nda_s3_interact.sh \"*/raw/*/surveys/*_psychs_*_fu*csv\" ls
 Run this script from PHOENIX/PROTECTED directory.
 Source aws-keys before running it.
 First arg is pattern, second arg is ls or rm"""
-        exit
-    fi
+    exit
+fi
+
+
+if [ -z $AWS_ACCESS_KEY_ID ] || [ -z $AWS_SECRET_ACCESS_KEY ]
+then
+    echo Define AWS_ACCESS_KEY_ID and AWS_SECRET_ACCESS_KEY and try again
+    exit
+fi
+
 
 for i in $1
 do
