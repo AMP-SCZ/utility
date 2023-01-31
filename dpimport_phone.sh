7ed4f6985ae32ee1c228b78d83617409b9b79321
Billah, Tashrif <35086881+tashrifbillah@users.noreply.github.com>
2023-01-31 18:00:54 -0500

    * phone_ and actigraphy_ need to remain as individual commands * added dpgvail and dppay in another command

--- dpimport_phone.sh
+++ dpimport_phone.sh
@@ -22,7 +22,18 @@ source /data/predict/utility/.vault/.env.${2}
 mongo --tls --tlsCAFile $state/ssl/ca/cacert.pem \
 --tlsCertificateKeyFile $state/ssl/mongo_client.pem \
 mongodb://dpdash:$MONGO_PASS@$HOST:$PORT/dpdata?authSource=admin \
---eval "assess=[\"phone_\",\"actigraphy_\",\"dpgvail\",\"dppay\"]" /data/predict/utility/remove_assess.js
+--eval "assess=\"phone_\"" /data/predict/utility/remove_assess.js
+
+mongo --tls --tlsCAFile $state/ssl/ca/cacert.pem \
+--tlsCertificateKeyFile $state/ssl/mongo_client.pem \
+mongodb://dpdash:$MONGO_PASS@$HOST:$PORT/dpdata?authSource=admin \
+--eval "assess=\"actigraphy_\"" /data/predict/utility/remove_assess.js
+
+mongo --tls --tlsCAFile $state/ssl/ca/cacert.pem \
+--tlsCertificateKeyFile $state/ssl/mongo_client.pem \
+mongodb://dpdash:$MONGO_PASS@$HOST:$PORT/dpdata?authSource=admin \
+--eval "assess=[\"dpgvail\",\"dppay\"]" /data/predict/utility/remove_assess.js
+
 echo ''
 
 
