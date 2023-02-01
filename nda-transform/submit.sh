d2cf217599ea907a937a517e3e8b818a3d7b717a
Billah, Tashrif <35086881+tashrifbillah@users.noreply.github.com>
2023-02-01 09:40:02 -0500

    write collection name within script

--- nda-transform/submit.sh
+++ nda-transform/submit.sh
@@ -2,25 +2,25 @@
 
 export PATH=/data/predict/miniconda3/bin:$PATH
 
-if [ $# -lt 3 ] || [ $1 = '-h' ] || [ $1 == '--help' ]
+if [ $# -lt 2 ] || [ $1 = '-h' ] || [ $1 == '--help' ]
 then
     echo """Usage:
-./submit.sh ndar_subject01 tashrif 1234
-./submit.sh ndar_subject01 tashrif 1234 Prescient
-Provide NDA dict name, submitter's NDA user name, and NDA collection ID
+./submit.sh ndar_subject01 tashrif
+./submit.sh ndar_subject01 tashrif Prescient
+Provide NDA dict name & submitter's NDA username
 Network name is optional. It is useful when you will have to retry just one submission."""
     exit
 fi
 
 form=$1
 user=$2
-collection=$3
+collection=PROD-AMPSCZ
 root=/data/predict1
 datestamp=$(date +"%Y%m%d")
 
-if [ ! -z $4 ]
+if [ ! -z $3 ]
 then
-    network=$4
+    network=$3
 else
     network="Pronet Prescient"
 fi
