c08efda58d30a0dfce842bea957548697d9988e0
Billah, Tashrif <35086881+tashrifbillah@users.noreply.github.com>
2023-02-01 10:04:04 -0500

    * use full path for renumber_days.py * remove old combined data * include open_count/*csv files

--- dpimport_avlqc.sh
+++ dpimport_avlqc.sh
@@ -23,7 +23,7 @@ source /data/predict/utility/.vault/.env.${2}
 mongo --tls --tlsCAFile $state/ssl/ca/cacert.pem \
 --tlsCertificateKeyFile $state/ssl/mongo_client.pem \
 mongodb://dpdash:$MONGO_PASS@$HOST:$PORT/dpdata?authSource=admin \
---eval "assess=[\"interviewMonoAudioQC_open\",\"interviewVideoQC_open\",\"interviewRedactedTranscriptQC_open\",\"interviewMonoAudioQC_psychs\",\"interviewMonoVideoQC_psychs\",\"interviewRedactedTranscriptQC_psychs\",\"avlqc\"]" /data/predict/utility/remove_assess.js
+--eval "assess=[\"interviewMonoAudioQC_open\",\"interviewVideoQC_open\",\"interviewRedactedTranscriptQC_open\",\"interviewMonoAudioQC_psychs\",\"interviewMonoVideoQC_psychs\",\"interviewRedactedTranscriptQC_psychs\",\"avlqc\", \"open_count\"]" /data/predict/utility/remove_assess.js
 echo ''
 
 
@@ -33,13 +33,16 @@ cd ${NDA_ROOT}
 import.py -c $CONFIG "*/PHOENIX/GENERAL/*/processed/*/interviews/*/??-*-interview*day*.csv"
 
 
-/data/predict/utility/combine_avlqc.py ${NDA_ROOT}
 cd AVL_quick_qc
+rm -rf *
+
+/data/predict/utility/combine_avlqc.py ${NDA_ROOT}
 assess=avlqc
 name=combined
 cat ${name}-PRONET-${assess}-day1to1.csv > ${name}-AMPSCZ-avlqc-day1to1.csv
 tail -n +2 ${name}-PRESCIENT-${assess}-day1to1.csv >> ${name}-AMPSCZ-${assess}-day1to1.csv
-renumber_days.py ${name}-AMPSCZ-${assess}-day1to1.csv
+/data/predict/utility/renumber_days.py ${name}-AMPSCZ-${assess}-day1to1.csv
 
 import.py -c $CONFIG "combined-*-avlqc-day1to1.csv"
+import.py -c $CONFIG "open_count/??-*-open_count-day1to*csv"
 
