594aa0c2ec85a6f9335931cf59ff5624214dfb84
Billah, Tashrif <35086881+tashrifbillah@users.noreply.github.com>
2023-02-22 16:08:30 -0500

    * add eeg, mri count * replace predict/python by predict1/python
    Co-authored-by: Kang Ik Kevin Cho <kevincho@bwh.harvard.edu>

--- dpimport_mriqc.sh
+++ dpimport_mriqc.sh
@@ -25,14 +25,25 @@ source /data/predict/utility/.vault/.env.${2}
 
 # remove old data
 mongo --tls --tlsCAFile $state/ssl/ca/cacert.pem --tlsCertificateKeyFile $state/ssl/mongo_client.pem mongodb://dpdash:$MONGO_PASS@$HOST:$PORT/dpdata?authSource=admin --eval "assess=[\"mriqc\"]" /data/predict/utility/remove_assess.js
+mongo --tls --tlsCAFile $state/ssl/ca/cacert.pem --tlsCertificateKeyFile $state/ssl/mongo_client.pem mongodb://dpdash:$MONGO_PASS@$HOST:$PORT/dpdata?authSource=admin --eval "assess=[\"eegcount\"]" /data/predict/utility/remove_assess.js
+mongo --tls --tlsCAFile $state/ssl/ca/cacert.pem --tlsCertificateKeyFile $state/ssl/mongo_client.pem mongodb://dpdash:$MONGO_PASS@$HOST:$PORT/dpdata?authSource=admin --eval "assess=[\"mricount\"]" /data/predict/utility/remove_assess.js
+mongo --tls --tlsCAFile $state/ssl/ca/cacert.pem --tlsCertificateKeyFile $state/ssl/mongo_client.pem mongodb://dpdash:$MONGO_PASS@$HOST:$PORT/dpdata?authSource=admin --eval "assess=[\"mriqcval\"]" /data/predict/utility/remove_assess.js
 
 # import new data
-export PATH=/data/predict/miniconda3/bin:$PATH
+export PATH=/data/predict1/miniconda3/bin:$PATH
 cd ${NDA_ROOT}
 
 # project level data
 import.py -c $CONFIG "MRI_ROOT/derivatives/quick_qc/combined-*-mriqc-day1to*.csv"
 
 # subject level data
-import.py -c $CONFIG "MRI_ROOT/derivatives/quick_qc/*/??-???????-mriqc-day1to*.csv"
+import.py -c $CONFIG "MRI_ROOT/derivatives/quick_qc/*/??-*-mriqc-day1to*.csv"
 
+# eeg count data : temporary
+import.py -c $CONFIG "MRI_ROOT/eeg_mri_count/??-*-eegcount-day1to*.csv"
+
+# mri count data : temporary
+import.py -c $CONFIG "MRI_ROOT/eeg_mri_count/??-*-mricount-day1to*.csv"
+
+# mri qc final data : temporary
+import.py -c $CONFIG "MRI_ROOT/eeg_mri_count/??-*-mriqcval-day1to*.csv"
