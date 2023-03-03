e7776345fb50a9baf2f209b8f2ffd2f37aa8e1e9
Billah, Tashrif <35086881+tashrifbillah@users.noreply.github.com>
2023-03-03 12:15:48 -0500

    a wrapper for NDA-compliant data generation

---
+++ nda-transform/generate.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+export PATH=/data/predict1/miniconda3/bin:$PATH
+
+_help()
+{
+    echo """Usage:
+./generate.sh -f nsipr -n Pronet -e baseline -p chrnsipr
+./generate.sh -f assist01 -n Prescient -e month_2 -p chrassist
+
+Mandatory:
+-f : NDA dict name 
+-n : network
+-e : event
+-p : variable name prefix e.g. chrnsipr, chrassist
+
+"""
+
+    exit
+}
+
+
+while getopts "f:n:e:p:" i
+do
+    case $i in
+        f) form=$OPTARG ;;
+        n) network=$OPTARG ;;
+        e) event=$OPTARG ;;
+        p) prefix=$OPTARG ;;
+        ?) _help ;;
+    esac
+done
+
+
+datestamp=$(date +"%Y%m%d")
+
+
+pushd .
+cd /data/predict1/to_nda/
+
+/data/predict1/utility/nda-transform/${form}.py --dict nda-templates/${form}_template.csv --root /data/predict1/data_from_nda/${network}/PHOENIX/GENERAL/ -t "*/processed/*/surveys/*.${network}.json" -o nda-submissions/${form}_${network}_${event}.csv --shared nda-submissions/ndar_subject01_${network}.csv -e ${event} -p $prefix
+
+popd
+
