3c4e32ff76d173355e4fb74b346f93f77d45a562
Billah, Tashrif <35086881+tashrifbillah@users.noreply.github.com>
2023-03-20 15:33:08 -0400

    print user list in csv format

--- get_dpdash_accounts.sh
+++ get_dpdash_accounts.sh
@@ -6,7 +6,10 @@
 cd /opt/dpdash/dpdash
 source singularity/.env
 prefix=dpdash_users_$(date +"%Y%m%d")
-/root/mongodb-linux-x86_64-rhel70-4.4.6/bin/mongo --tls --tlsCAFile $state/ssl/ca/cacert.pem --tlsCertificateKeyFile $state/ssl/mongo_client.pem mongodb://dpdash:$MONGO_PASS@`hostname`:27017/dpdmongo?authSource=admin --eval "db.users.find().forEach(u=>print(u.display_name+','+u.uid+','+u.mail))" > users/$prefix.txt
+
+echo Name,Username,Email > users/$prefix.csv
+
+/root/mongodb-linux-x86_64-rhel70-4.4.6/bin/mongo --tls --tlsCAFile $state/ssl/ca/cacert.pem --tlsCertificateKeyFile $state/ssl/mongo_client.pem mongodb://dpdash:$MONGO_PASS@`hostname`:27017/dpdmongo?authSource=admin --eval "db.users.find().forEach(u=>print(u.display_name+','+u.uid+','+u.mail))" | tail -n +5 >> users/$prefix.csv
 
 for to in $@
 do
