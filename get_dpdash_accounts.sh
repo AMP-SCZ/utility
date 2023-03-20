c5ab77083c478e0206b5ee0d9a2c4703831e6837
Billah, Tashrif <35086881+tashrifbillah@users.noreply.github.com>
2023-03-20 15:19:02 -0400

    use full path from mongo

--- get_dpdash_accounts.sh
+++ get_dpdash_accounts.sh
@@ -6,7 +6,7 @@
 cd /opt/dpdash/dpdash
 source singularity/.env
 prefix=dpdash_users_$(date +"%Y%m%d")
-mongo --tls --tlsCAFile $state/ssl/ca/cacert.pem --tlsCertificateKeyFile $state/ssl/mongo_client.pem mongodb://dpdash:$MONGO_PASS@`hostname`:27017/dpdmongo?authSource=admin --eval "db.users.find().forEach(u=>print(u.display_name+','+u.uid+','+u.mail))" > users/$prefix.txt
+/root/mongodb-linux-x86_64-rhel70-4.4.6/bin/mongo --tls --tlsCAFile $state/ssl/ca/cacert.pem --tlsCertificateKeyFile $state/ssl/mongo_client.pem mongodb://dpdash:$MONGO_PASS@`hostname`:27017/dpdmongo?authSource=admin --eval "db.users.find().forEach(u=>print(u.display_name+','+u.uid+','+u.mail))" > users/$prefix.txt
 
 for to in $@
 do
