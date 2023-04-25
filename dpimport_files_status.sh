407c753150e26b5b65f0cd24dbb0e9b51381e11d
Billah, Tashrif <35086881+tashrifbillah@users.noreply.github.com>
2023-04-25 17:51:45 -0400

    start tracking data_baseline,data_month_2 files

--- dpimport_files_status.sh
+++ dpimport_files_status.sh
@@ -22,7 +22,7 @@ echo Importing to mongodb://dpdash:MONGO_PASS@$HOST:$PORT
 echo ''
 
 # delete old collections
-mongo --tls --tlsCAFile $state/ssl/ca/cacert.pem --tlsCertificateKeyFile $state/ssl/mongo_client.pem mongodb://dpdash:$MONGO_PASS@$HOST:$PORT/dpdata?authSource=admin --eval "assess=[\"flowcheck\"]" /data/predict1/utility/remove_assess.js
+mongo --tls --tlsCAFile $state/ssl/ca/cacert.pem --tlsCertificateKeyFile $state/ssl/mongo_client.pem mongodb://dpdash:$MONGO_PASS@$HOST:$PORT/dpdata?authSource=admin --eval "assess=[\"flowcheck\",\"data_baseline\",\"data_month_2\"]" /data/predict1/utility/remove_assess.js
 
 # delete metadata
 mongo --tls --tlsCAFile $state/ssl/ca/cacert.pem --tlsCertificateKeyFile $state/ssl/mongo_client.pem mongodb://dpdash:$MONGO_PASS@$HOST:$PORT/dpdata?authSource=admin /data/predict1/utility/remove_metadata.js
@@ -40,6 +40,10 @@ import.py -c $CONFIG "*_status/*_metadata.csv"
 import.py -c $CONFIG combined-AMPSCZ-flowcheck-day1to1.csv
 import.py -c $CONFIG "*_status/combined-*-flowcheck-day1to1.csv"
 
+import.py -c $CONFIG "combined-AMPSCZ-data_*-day1to1.csv"
+import.py -c $CONFIG "*_status/combined-*-data_*-day1to1.csv"
+
 # subject level files status
 import.py -c $CONFIG "*_status/??-*-flowcheck-day1to1.csv"
+import.py -c $CONFIG "*_status/??-*-data_*-day1to1.csv"
 
