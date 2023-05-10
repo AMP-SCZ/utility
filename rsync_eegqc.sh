61a2c1585357fd1e8a570c93de4ad22bed2ddd85
Billah, Tashrif <35086881+tashrifbillah@users.noreply.github.com>
2023-05-10 17:21:00 -0400

    sync every Figures directory

--- rsync_eegqc.sh
+++ rsync_eegqc.sh
@@ -2,7 +2,9 @@
 
 NDA_ROOT=$1
 cd $NDA_ROOT
-rsync -avR */PHOENIX/PROTECTED/*/processed/*/eeg/*/Figures/*[!QC].png \
--avR */PHOENIX/PROTECTED/*/processed/*/eeg/*/Figures/*_runSheet.* \
-rc-predict-gen.partners.org:$2
 
+for d in `ls -d */PHOENIX/PROTECTED/*/processed/*/eeg/*/Figures`
+do
+    rsync -avR ${d}/*[!QC].png rc-predict-gen.partners.org:$2
+    rsync -avR ${d}/*_runSheet.* rc-predict-gen.partners.org:$2
+done
