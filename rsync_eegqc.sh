21278e782293e59ee5b3e3e15a317a847a83204c
Billah, Tashrif <35086881+tashrifbillah@users.noreply.github.com>
2023-05-10 17:28:52 -0400

    rsync at site level

--- rsync_eegqc.sh
+++ rsync_eegqc.sh
@@ -3,8 +3,10 @@
 NDA_ROOT=$1
 cd $NDA_ROOT
 
-for d in `ls -d */PHOENIX/PROTECTED/*/processed/*/eeg/*/Figures`
+for d in `ls -d */PHOENIX/PROTECTED/*/`
 do
-    rsync -avR ${d}/*[!QC].png rc-predict-gen.partners.org:$2
-    rsync -avR ${d}/*_runSheet.* rc-predict-gen.partners.org:$2
+    rsync -avR ${d}/processed/*/eeg/*/Figures/*[!QC].png rc-predict-gen.partners.org:$2
+    rsync -avR ${d}/processed/*/eeg/*/Figures/*_runSheet.* rc-predict-gen.partners.org:$2
 done
+
+
