03e20d527d3af7132b4ac3a94be12b8284d3fb92
Billah, Tashrif <35086881+tashrifbillah@users.noreply.github.com>
2023-05-26 15:54:41 -0400

    rename flatten_multi_records_1.py -> flatten_multi_records.py

--- _flatten_multi_records.sh
+++ _flatten_multi_records.sh
@@ -10,27 +10,27 @@ export PATH=/data/predict1/miniconda3/bin/:/data/predict1/utility/:$PATH
 
 # unique visit, single file
 # floating (99)
-flatten_multi_records_1.py ${1}_past_pharmaceutical_treatment.csv
-flatten_multi_records_1.py ${1}_psychosocial_treatment_form.csv
-flatten_multi_records_1.py ${1}_health_conditions_medical_historypsychiatric_histo.csv
+flatten_multi_records.py ${1}_past_pharmaceutical_treatment.csv
+flatten_multi_records.py ${1}_psychosocial_treatment_form.csv
+flatten_multi_records.py ${1}_health_conditions_medical_historypsychiatric_histo.csv
 
 # unique visit, multiple files
 # screening (1)
-flatten_multi_records_1.py ${1}_family_interview_for_genetic_studies_figs.csv ${1}_family_interview_for_genetic_studies_figs_child.csv ${1}_family_interview_for_genetic_studies_figs_sibling.csv
+flatten_multi_records.py ${1}_family_interview_for_genetic_studies_figs.csv ${1}_family_interview_for_genetic_studies_figs_child.csv ${1}_family_interview_for_genetic_studies_figs_sibling.csv
 
-flatten_multi_records_1.py ${1}_traumatic_brain_injury_screen.csv ${1}_traumatic_brain_injury_screen_parent.csv ${1}_traumatic_brain_injury_screen_subject.csv
+flatten_multi_records.py ${1}_traumatic_brain_injury_screen.csv ${1}_traumatic_brain_injury_screen_parent.csv ${1}_traumatic_brain_injury_screen_subject.csv
 
 
 
 # non-unique visit, single file
 # all visits
-flatten_multi_records_1.py ${1}_current_pharmaceutical_treatment_floating_med_125.csv
-flatten_multi_records_1.py ${1}_current_pharmaceutical_treatment_floating_med_2650.csv
-flatten_multi_records_1.py ${1}_adverse_events.csv
-flatten_multi_records_1.py ${1}_resource_use_log.csv
+flatten_multi_records.py ${1}_current_pharmaceutical_treatment_floating_med_125.csv
+flatten_multi_records.py ${1}_current_pharmaceutical_treatment_floating_med_2650.csv
+flatten_multi_records.py ${1}_adverse_events.csv
+flatten_multi_records.py ${1}_resource_use_log.csv
 
 
 # non-unique visit, multiple files
 # baseline (2) and month 2 (4)
-flatten_multi_records_1.py ${1}_blood_sample_preanalytic_quality_assurance.csv ${1}_blood_sample_preanalytic_quality_assurance_bc.csv ${1}_blood_sample_preanalytic_quality_assurance_pl.csv ${1}_blood_sample_preanalytic_quality_assurance_se.csv ${1}_blood_sample_preanalytic_quality_assurance_wb.csv
+flatten_multi_records.py ${1}_blood_sample_preanalytic_quality_assurance.csv ${1}_blood_sample_preanalytic_quality_assurance_bc.csv ${1}_blood_sample_preanalytic_quality_assurance_pl.csv ${1}_blood_sample_preanalytic_quality_assurance_se.csv ${1}_blood_sample_preanalytic_quality_assurance_wb.csv
 
