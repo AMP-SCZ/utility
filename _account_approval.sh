32bf890931be2973cd0b8bc4422cbf28910d7e84
Billah, Tashrif <35086881+tashrifbillah@users.noreply.github.com>
2023-08-10 11:18:00 -0400

    * help message with multiple users on the command line * keep a separate commented block for configuring all accounts at a time

--- _account_approval.sh
+++ _account_approval.sh
@@ -3,17 +3,20 @@
 # It is run within VM as root
 # Usage:
 # ./_account_approval.sh abc123@partners.org
+# ./_account_approval.sh abc123@partners.org def456@gmail.com
 # ./_account_approval.sh list.txt
 # Where list.txt contains:
 # abc123@partners.org
-# def456@gmail.com
+# def456@gmail.org
 # ...
 
 source /opt/dpdash/dpdash/singularity/.env
-mongo --tls --tlsCAFile $state/ssl/ca/cacert.pem \
---tlsCertificateKeyFile $state/ssl/mongo_client.pem \
-mongodb://dpdash:$MONGO_PASS@`hostname`:27017/dpdmongo?authSource=admin \
-/opt/dpdash/dpdash/_configure_account.mongo.js
+
+# configure all registered accounts
+# mongo --tls --tlsCAFile $state/ssl/ca/cacert.pem \
+# --tlsCertificateKeyFile $state/ssl/mongo_client.pem \
+# mongodb://dpdash:$MONGO_PASS@`hostname`:27017/dpdmongo?authSource=admin \
+# /opt/dpdash/dpdash/_configure_account.mongo.js
 
 
 if [ -f $1 ]
@@ -26,8 +29,15 @@ fi
 for e in $recepients
 do
     echo $e
+    
+    # configure one (selective) account at a time
+    mongo --tls --tlsCAFile $state/ssl/ca/cacert.pem \
+    --tlsCertificateKeyFile $state/ssl/mongo_client.pem \
+    mongodb://dpdash:$MONGO_PASS@`hostname`:27017/dpdmongo?authSource=admin \
+    --eval "user=\"$e\"" \
+    /opt/dpdash/dpdash/_configure_account1.mongo.js
+
     cat account_approval.txt | mailx -s "DPdash account approved" -r tbillah@partners.org \
     -c sylvain.bouix@etsmtl.ca -c jtbaker@partners.org -c tkapur@partners.org -- $e
 done
-
 
