d827010fa9eb531d417256f625d6d8e0149e97ad
Billah, Tashrif <35086881+tashrifbillah@users.noreply.github.com>
2023-09-08 17:19:42 -0400

    typo

--- chmod_chgrp.sh
+++ chmod_chgrp.sh
@@ -34,7 +34,7 @@ echo Not printing directory names for speed
 IFS=$'\n'
 for d in $(cat $list)
 do
-    g=`ls -ld ./PROTECTED/PronetYA/processed/YA11278/phone | cut -d ' ' -f 4`
+    g=`ls -ld $d | cut -d ' ' -f 4`
     if [[ "$g" != "$g1" ]]
     then
         chgrp $g1 $d
