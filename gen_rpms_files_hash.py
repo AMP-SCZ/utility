0e9f04dd039cdc45c307b47678b138d45770a07b
Billah, Tashrif <35086881+tashrifbillah@users.noreply.github.com>
2023-09-15 12:22:53 -0400

    generate individual file hashes for RPMS: first pass

---
+++ gen_rpms_files_hash.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python
+
+import sys
+import hashlib
+import pandas as pd
+from os.path import abspath, isfile
+
+if len(sys.argv)<2 or sys.argv[1] in ['-h','--help']:
+    print(f'''Usage:
+    {abspath(__file__)} ME57953
+execute it within /path/to/ME57953/surveys/ directory
+''')
+    exit(0)
+
+
+subject=sys.argv[1]
+
+# read or create hash record
+hashfile=f'{subject}_hashes.csv'
+if isfile(hashfile):
+    df=pd.read_csv(hashfile)
+else:
+    df=pd.DataFrame(columns='form content_hash upload'.split())
+
+df.set_index('form',inplace=True)
+
+
+
+with open('/data/predict1/utility/rpms_file_suffix.txt') as f:
+    suffixes=f.read().split()
+
+
+# read files, compute and compare hashes
+for suffix in suffixes:
+    form=f'{subject}_{suffix}'
+    
+    if isfile(form):
+        with open(form) as f:
+            content=f.read().strip()
+            content_hash = hashlib.sha256(content.encode()).hexdigest()
+        
+        # new file
+        try:
+            df.loc[form]
+        except:
+            df.loc[form]=[content_hash,1]
+
+        # changed determiner
+        if df.loc[form,'content_hash']!=content_hash:
+            df.loc[form]=[content_hash,1]
+        else:
+            df.at[form,'upload']=0
+
+
+df.to_csv(hashfile)
+
+
