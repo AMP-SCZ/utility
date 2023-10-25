45aa0516f40274c160c66ca5f37a1b9532fbb3d2
Billah, Tashrif <35086881+tashrifbillah@users.noreply.github.com>
2023-10-25 15:12:15 -0400

    change predict-->predict1

--- _crontab.sh
+++ _crontab.sh
@@ -6,13 +6,13 @@
 # === hna002 ===
 
 # generate lochness datalake's files status at 6 am and 9pm for dpdash
-30 6,21 * * * * /data/predict/utility/generate_files_status.sh /data/predict/data_from_nda_dev/ dpstage
+30 6,21 * * * * /data/predict1/utility/generate_files_status.sh /data/predict1/data_from_nda_dev/ dpstage
 
 # import AVL QC data, daily at 10 pm
-45 22 * * * /data/predict/utility/dpimport_avlqc.sh /data/predict/data_from_nda_dev/ dpstage
+45 22 * * * /data/predict1/utility/dpimport_avlqc.sh /data/predict1/data_from_nda_dev/ dpstage
 
 # generate difference between AMP-SCZ and network data dictionaries
-0 6 * * * /data/predict/utility/_gen_dict_diff.sh 123456 123456 123456 tbillah sbouix gjacobs1
+0 6 * * * /data/predict1/utility/_gen_dict_diff.sh 123456 123456 123456 tbillah sbouix gjacobs1
 
 
 
@@ -22,7 +22,7 @@
 0 0 * * 1 /data/predict1/utility/track_briefcase_size.sh /data/predict1/ tbillah sbouix jtbaker
 
 # clear REDCap upload logs, every Friday at 5 pm
-0 17 * * 5 rm -f /data/predict/utility/bsub/*
+0 17 * * 5 rm -f /data/predict1/utility/bsub/*
 
 # clear NDA upload logs every Monday at 12 am
 0 0 * * 1 rm -f /PHShome/tb571/NDA/nda-tools/vtcmd/*/*
@@ -31,27 +31,27 @@
 # used to be 0 */3, 15 */3, 30 */3, 45 */3 (every three hours)
 # changed to only once a week to unburden the compute nodes
 # PRESCIENT mock
-0 0 * * 0 /data/predict/utility/_rpms_to_redcap.sh /data/predict/data_from_nda_dev/Prescient/PHOENIX/PROTECTED /data/predict/utility/amp-scz-form 123456
+0 0 * * 0 /data/predict1/utility/_rpms_to_redcap.sh /data/predict1/data_from_nda_dev/Prescient/PHOENIX/PROTECTED /data/predict1/utility/amp-scz-form 123456
 
 # ProNET mock
-0 0 * * 2 /data/predict/utility/_records_to_redcap.sh /data/predict/data_from_nda_dev/Pronet/PHOENIX/PROTECTED /data/predict/utility/redcap-ii-yale 123456
+0 0 * * 2 /data/predict1/utility/_records_to_redcap.sh /data/predict1/data_from_nda_dev/Pronet/PHOENIX/PROTECTED /data/predict1/utility/redcap-ii-yale 123456
 
 
 # === dn001 ===
 
 # PRESCIENT real
 # determine if new and upload to REDCap
-# 0 21 * * 2 /data/predict/utility/set_rpms_date_shifts.py /data/predict1/data_from_nda/Prescient/PHOENIX/PROTECTED/ "*/raw/*/surveys/" && /data/predict/utility/_rpms_to_redcap.sh /data/predict1/data_from_nda/Prescient/PHOENIX/PROTECTED /data/predict/utility/rpms-to-yale 123456
+0 21 * * 2 /data/predict1/utility/set_rpms_date_shifts.py /data/predict1/data_from_nda/Prescient/PHOENIX/PROTECTED/ "*/raw/*/surveys/" && /data/predict1/utility/_rpms_to_redcap.sh /data/predict1/data_from_nda/Prescient/PHOENIX/PROTECTED /data/predict1/utility/rpms-to-yale 123456
 
 # keep one day difference between upload and download so upload can complete
 
 # download from REDCap and shift dates
-# 0 21 * * 4 /data/predict/utility/down_mgb_redcap_records.py /data/predict1/data_from_nda/Prescient/PHOENIX/PROTECTED/ 123456 && /data/predict/utility/shift_redcap_dates.py /data/predict1/data_from_nda/Prescient/PHOENIX/PROTECTED/ "*/raw/*/surveys/*.Prescient.json" /data/predict1/utility/rpms-to-yale/CloneOfYaleRealRecords_DataDictionary_2022-12-26_calc_to_text_checkbox.csv
+0 21 * * 4 /data/predict1/utility/down_mgb_redcap_records.py /data/predict1/data_from_nda/Prescient/PHOENIX/PROTECTED/ 123456 && /data/predict1/utility/shift_redcap_dates.py /data/predict1/data_from_nda/Prescient/PHOENIX/PROTECTED/ "*/raw/*/surveys/*.Prescient.json" /data/predict1/utility/rpms-to-yale/CloneOfYaleRealRecords_DataDictionary_2022-12-26_calc_to_text_checkbox.csv
 
 # ProNET real
 # determine if new and shift dates
 # determine if new and upload to REDCap
-# 00 22 * * * /data/predict/utility/_shift_redcap_dates.sh /data/predict1/data_from_nda/Pronet/PHOENIX/PROTECTED "*/raw/*/surveys/*.Pronet.json" /data/predict/utility/yale-real/CloneOfYaleRealRecords_DataDictionary_2022-12-26_checkbox.csv && /data/predict/utility/_records_to_redcap.sh /data/predict1/data_from_nda/Pronet/PHOENIX/PROTECTED /data/predict/utility/yale-real 123456
+00 22 * * * /data/predict1/utility/_shift_redcap_dates.sh /data/predict1/data_from_nda/Pronet/PHOENIX/PROTECTED "*/raw/*/surveys/*.Pronet.json" /data/predict1/utility/yale-real/CloneOfYaleRealRecords_DataDictionary_2022-12-26_checkbox.csv && /data/predict1/utility/_records_to_redcap.sh /data/predict1/data_from_nda/Pronet/PHOENIX/PROTECTED /data/predict1/utility/yale-real 123456
 
 
 # mongodb backup
@@ -61,17 +61,17 @@
 
 # download EEG run sheets
 # every morning at 3 am
-0 3 * * * /data/predict/eeg-qc-dash/down_eeg_pdf_rsheet.sh 123456 /data/predict/data_from_nda/Pronet
+0 3 * * * /data/predict1/eeg-qc-dash/down_eeg_pdf_rsheet.sh 123456 /data/predict1/data_from_nda/Pronet
 # every morning at 4 am
-0 4 * * * /data/predict/eeg-qc-dash/down_eeg_pdf_rsheet.sh 123456 /data/predict/data_from_nda/Prescient
+0 4 * * * /data/predict1/eeg-qc-dash/down_eeg_pdf_rsheet.sh 123456 /data/predict1/data_from_nda/Prescient
 
 
 
 # === dn020 ===
 
 # copy EEG QC images over to web app VM
-0 4 * * * /data/predict/utility/rsync_eegqc.sh /data/predict/data_from_nda/ /data/eegqc/
-0 5 * * * /data/predict/utility/rsync_eegqc.sh /data/predict/kcho/flow_test/spero/ /opt/data/eegqc-mock/
+0 4 * * * /data/predict1/utility/rsync_eegqc.sh /data/predict1/data_from_nda/ /data/eegqc/
+0 5 * * * /data/predict1/utility/rsync_eegqc.sh /data/predict1/kcho/flow_test/spero/ /opt/data/eegqc-mock/
 
 0 05 * * * /data/predict1/utility/backup_formqc.sh
 
