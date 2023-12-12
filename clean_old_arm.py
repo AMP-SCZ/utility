092f8b58348b0d85b27d9c85458b3374579b61b8
Billah, Tashrif <35086881+tashrifbillah@users.noreply.github.com>
2023-12-12 17:19:11 -0500

    script for cleaning up PRESCIENT arm, first pass

---
+++ clean_old_arm.py
@@ -0,0 +1,64 @@
+#!/usr/bin/env python
+
+import sys
+import pandas as pd
+from glob import glob
+
+
+with open('bsub/rpms_records.txt') as f:
+    dirs= f.read().strip().split()
+
+ROOTDIR=sys.argv[1]
+
+print('\n\n')
+
+for dir in dirs:
+
+    print(dir)
+    chdir(dir)
+    subjectkey= dir.split('/')[2]
+
+    incl_excl= subjectkey+ '_inclusionexclusion_criteria_review.csv'
+    inform_consent= subjectkey+ '_informed_consent_run_sheet.csv'
+
+    if isfile(inform_consent):
+        df= pd.read_csv(inform_consent)
+        # extract Young Patient's rows only, we do not need Guardian's rows
+        yp_rows= df[df['version']=='YP']
+        _chr_hc= yp_rows['group'].unique()
+        # to account for re-consent scenario, consider only the last row
+        chr_hc= yp_rows.iloc[-1]['group']
+
+    else:
+        continue
+    
+
+    # one try-except block to handle absence of incl_excl and empty chrcrit_part
+    try:
+        df= pd.read_csv(incl_excl)
+        chrit_part= int(df['chrcrit_part'])
+
+        if chrcrit_part!=chr_hc:
+            if chrcrit_part==1:
+                old=2
+            else:
+                old=1
+
+
+    except (FileNotFoundError,ValueError):
+
+        if len(_chr_hc)>1:
+            if chr_hc=='UHR':
+                old=2
+            elif chr_hc=='HealthyControl':
+                old=1
+        
+    
+    print(f'old arm: {old}')
+
+
+    chdir(ROOTDIR)
+
+
+print('\n\n')
+
