f78913f9193258e7138af0ea9c3372d07cc062ca
Billah, Tashrif <35086881+tashrifbillah@users.noreply.github.com>
2024-02-19 22:54:19 -0500

    decouple shift_redcap_dates.py from newgrp BWH-PREDICT-G

--- _shift_redcap_dates.sh
+++ _shift_redcap_dates.sh
@@ -9,7 +9,7 @@ JSON=$2
 
 cd /data/predict1/utility/
 ./set_date_shifts.py $PHOENIX_PROTECTED $JSON
-umask 007 && newgrp BWH-PREDICT-G && \
+umask 007 && newgrp BWH-PREDICT-G
 ./shift_redcap_dates.py $PHOENIX_PROTECTED $JSON $3
 
 
