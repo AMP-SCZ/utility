c825e2a496c426006127a85f7fd38abeb848dad8
Billah, Tashrif <35086881+tashrifbillah@users.noreply.github.com>
2024-02-23 16:08:48 -0500

    * use DATA_FILE_SUFFIX variable so we won't have to modify the script * enhance help message

--- nda-transform/_langsamp01.sh
+++ nda-transform/_langsamp01.sh
@@ -5,14 +5,16 @@ export PATH=/data/predict1/miniconda3/bin/:$PATH
 _help()
 {
     echo """Usage:
-$0 TYPE
+$0 TYPE DATA_FILE_SUFFIX
 Accepted types are OPEN or PSYCHS only
+Keep input data files within nda-submissions/langsamp01/ folder and name them as {network}_{TYPE}_{DATA_FILE_SUFFIX}
+Example DATA_FILE_SUFFIX are: inventory.csv or inventory_no_prescreen.csv
 """
     exit
 }
 
 
-if [ $# == 0 ] || [ "$1" == "-h" ]
+if [ $# -lt 2 ] || [ "$1" == "-h" ]
 then
     _help
 fi
@@ -28,6 +30,7 @@ else
     _help
 fi
 
+data_file_suffix=$2
 
 for n in Pronet Prescient
 do
@@ -36,7 +39,7 @@ do
     do
         echo $e
         ./generate.sh -f langsamp01 -n $n -e $e -p $prefix \
-        -o "--data /data/predict1/to_nda/nda-submissions/langsamp01/${n}_${1}_inventory_no_prescreen.csv"
+        -o "--data /data/predict1/to_nda/nda-submissions/langsamp01/${n}_${1}_${data_file_suffix}"
         echo
     done
 done
