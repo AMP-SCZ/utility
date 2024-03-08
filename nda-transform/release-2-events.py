1e8f94d38e478be20cf9007847eb4f95b462c547
Billah, Tashrif <35086881+tashrifbillah@users.noreply.github.com>
2024-03-08 16:05:10 -0500

    script for defining release-2 data generation commands

---
+++ nda-transform/release-2-events.py
@@ -0,0 +1,22 @@
+import pandas as pd
+
+devents=pd.read_csv('/data/predict1/to_nda/nda-submissions/release-2-events')
+redcap_nda=pd.read_csv('/data/predict1/utility/nda-transform/redcap_nda_form_mapping.csv')
+redcap_nda.set_index('redcap_form_name',inplace=True)
+
+for i,row in devents.iterrows():
+    event=row['event'].split('_arm_')[0]
+    form=row['form']
+    
+    try:
+        short_name=redcap_nda.loc[form,'nda_short_name']
+        
+        if isinstance(short_name, str):
+            print('-f',short_name,'-e',event)
+        else:
+            # multiple rows for one form
+            print('\t',form)
+            continue
+    except KeyError:
+        pass
+
