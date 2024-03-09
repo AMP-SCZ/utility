5b0fddc1a65ce32a8a0991d4bb74656f9361f876
Billah, Tashrif <35086881+tashrifbillah@users.noreply.github.com>
2024-03-09 13:58:17 -0500

    pretty print

--- nda-transform/release-2-events.py
+++ nda-transform/release-2-events.py
@@ -3,6 +3,8 @@
 import pandas as pd
 
 devents=pd.read_csv('/data/predict1/to_nda/nda-submissions/release-2-events')
+# to group same form together
+# devents.sort_values('form',inplace=True)
 redcap_nda=pd.read_csv('/data/predict1/utility/nda-transform/redcap_nda_form_mapping.csv')
 redcap_nda.set_index('redcap_form_name',inplace=True)
 
@@ -15,12 +17,15 @@ for i,row in devents.iterrows():
 
         for _,_row in df.iterrows():
             addl_arg=_row['addl_arg']
-
-            if pd.isna(addl_arg):
-                print('-f',_row['nda_short_name'],'-e',event)
-            else:
-                print('-f',_row['nda_short_name'],'-e',event,'-p',addl_arg)
-        
+            short_name=_row['nda_short_name']
+            
+            cmd=f'./generate.sh -n $n -e {event:9} -f {short_name:16} '
+            
+            if not pd.isna(addl_arg):
+                cmd+=f'-p {addl_arg}'
+            
+            print(cmd)
+            
     except KeyError:
         pass
 
