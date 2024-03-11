62f23b39502d1a74418f980605086fc0001146b1
Billah, Tashrif <35086881+tashrifbillah@users.noreply.github.com>
2024-03-11 17:23:35 -0400

    model after combine_networks.sh for pipelining purpose

--- nda-transform/wais_iv_part101.sh
+++ nda-transform/wais_iv_part101.sh
@@ -1,14 +1,29 @@
 #!/bin/bash
 
-if [ "$1" == "-h" ]
-then
-    echo Usage: $0 /path/to/wisc_v01_*csv
+_help()
+{
+    echo """Usage:
+$0 -e baseline
+$0 -f /path/to/wisc_v01_*csv
+
+if -e is provided, network_combined/wisc_v01_baseline.csv is used as -f
+"""
+
     exit
-elif [ $# -lt 1 ]
+}
+
+while getopts "e:f:" i
+do
+    case $i in
+        e) event=$OPTARG ;;
+        f) file=$OPTARG ;;
+        ?) _help ;;
+    esac
+done
+
+if [ ! -z $event ]
 then
-    file=/data/predict1/to_nda/nda-submissions/network_combined/wisc_v01_baseline.csv
-else
-    file=$1
+    file=/data/predict1/to_nda/nda-submissions/network_combined/wisc_v01_${event}.csv
 fi
 
 newfile=${file/wisc_v/wais_iv_part1}
