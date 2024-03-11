7ebeda5f5b554beb6601ff2e83bebbf8e1de0413
Billah, Tashrif <35086881+tashrifbillah@users.noreply.github.com>
2024-03-11 15:04:47 -0400

    make input optional for pipelinining purpose

--- nda-transform/wais_iv_part101.sh
+++ nda-transform/wais_iv_part101.sh
@@ -1,12 +1,17 @@
 #!/bin/bash
 
-if [ $# -lt 1 ]
+if [ "$1" == "-h" ]
 then
     echo Usage: $0 /path/to/wisc_v01_*csv
     exit
+elif [ $# -lt 1 ]
+then
+    file=/data/predict1/to_nda/nda-submissions/network_combined/wisc_v01_baseline.csv
+else
+    file=$1
 fi
 
-newfile=${1/wisc_v/wais_iv_part1}
+newfile=${file/wisc_v/wais_iv_part1}
 
-sed "s/wisc_v/wais_iv_part1/g" $1 > $newfile
+sed "s/wisc_v/wais_iv_part1/g" $file > $newfile
 
