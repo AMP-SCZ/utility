e5c77e1d7c0f91d0ad3ac4f3713c67ef7a7520ba
Billah, Tashrif <35086881+tashrifbillah@users.noreply.github.com>
2024-03-29 12:08:58 -0400

    * do not hard code columns, read via glob() * include help message * increase form print field * accept output as an argument * remove previous output if exists

--- nda-transform/prepare_release_record.py
+++ nda-transform/prepare_release_record.py
@@ -2,12 +2,25 @@
 
 import pandas as pd
 from glob import glob
+import sys
+from os.path import isfile
+from os import remove
+
+if len(sys.argv)<2 or sys.argv[1] in ['-h','--help']:
+    print(f'''Usage:
+Execute this script within to_nda/nda-submissions/network_combined/ directory:
+{__file__} output.csv
+Output is ampscz-release-1-record.csv, ampscz-release-2-record.csv, etc.
+''')
+    exit()
+
+if isfile(sys.argv[1]):
+    remove(sys.argv[1])
 
 dfshared=pd.read_csv('ndar_subject01.csv',header=1)
 dfshared.set_index('src_subject_id',inplace=True)
 
-# columns=glob('*csv')
-columns=['actirec01.csv', 'ampscz_dim01.csv', 'ampscz_hcgfb01_screening.csv', 'ampscz_iqa01_baseline.csv', 'ampscz_lapes01_screening.csv', 'ampscz_nsipr01_baseline.csv', 'ampscz_psychs01_baseline.csv', 'ampscz_psychs01_screening.csv', 'ampscz_rap01_baseline.csv', 'ampscz_rs01.csv', 'ampscz_sp_sensors01_3705.csv', 'ampscz_sp_sensors01_4366.csv', 'ampscz_sp_survey01.csv', 'assist01_baseline.csv', 'bprs01_baseline.csv', 'cgis01_baseline.csv', 'clgry01_baseline.csv', 'cssrs01_baseline.csv', 'dsm_iv_es01_baseline.csv', 'dsm_iv_es01_screening.csv', 'eeg_sub_files01_baseline.csv', 'figs01.csv', 'gfs01_chrgfrs.csv', 'gfs01_chrgfss.csv', 'iec01.csv', 'image03_baseline.csv', 'langsamp01_baseline_open.csv', 'ndar_subject01.csv', 'oasis01_baseline.csv', 'pds01.csv', 'pmod01_baseline.csv', 'pss01_baseline.csv', 'scidvapd01_screening.csv', 'socdem01.csv', 'sri01_baseline.csv', 'tbi01_screening.csv', 'wais_iv_part101_baseline.csv', 'wasi201_baseline.csv', 'wisc_v01_baseline.csv']
+columns=sorted(glob('*csv'))
 
 dfupload=pd.DataFrame(columns=['src_subject_id']+columns)
 dfupload['src_subject_id']=dfshared.index
@@ -27,7 +40,7 @@ for file in columns:
         except:
             pass
     
-    print(f'{file:30}',count)
+    print(f'{file:32}',count)
 
-dfupload.to_csv('ampscz-release-1.0-record.csv')
+dfupload.to_csv(sys.argv[1])
 
