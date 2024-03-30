dc891a545b14520d812cb9015ffb8190cb125b32
Billah, Tashrif <35086881+tashrifbillah@users.noreply.github.com>
2024-03-30 06:28:32 -0400

    * accept id and collection as args * loop over various data file columns * count existent files

--- nda-transform/count_assoc_files.py
+++ nda-transform/count_assoc_files.py
@@ -1,22 +1,38 @@
+#!/usr/bin/env python
+
 from pathlib import Path
 import pandas as pd
 
-source_path = Path("/volumes/prod-ampscz/collaboration-space/3705")
 nda_submissions_path = Path("/volumes/prod-ampscz/nda-submissions")
-id = 59959
+id = sys.argv[1]
+collection = sys.argv[2]
+
+source_path = Path(f"/volumes/prod-ampscz/collaboration-space/{collection}")
 
 files = source_path.glob(f"*/csv/{id}/part-*csv")
 
+file_columns='data_file1 data_file2 image_file bvalfile bvecfile'.split()
+
+count=0
 for file in files:
     df = pd.read_csv(file)
-    if "data_file1" in df.columns:
+    
+    submission_folder = df.loc[0]["SUBMISSION_FOLDER_NAME"]
+    
+    for idx, row in df.iterrows():
+        
+        for c in file_columns:
+        
+            if c in df.columns:
+                if pd.isna(row[c]):
+                    data_file = row[c]
 
-        for idx, row in df.iterrows():
-            data_file = row["data_file1"]
-            submission_folder = row["SUBMISSION_FOLDER_NAME"]
+                    data_file_path: Path = nda_submissions_path / submission_folder / data_file
 
-            data_file_path: Path = nda_submissions_path / submission_folder / data_file 
+                    if not data_file_path.exists():
+                        print(f"File does not exist: {data_file_path}")
+                    else:
+                        count+=1
 
-            if not data_file_path.exists():
-                print(f"File does not exist: {data_file_path}")
-                break
+print('Total files', count)
+
