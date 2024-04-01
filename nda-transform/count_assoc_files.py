3fd518c255e040f44620c511697ebaaef8dc7f50
Dheshan Mohandass <dheshan@mohandass.com>
2024-04-01 14:43:35 -0400

    feat.: Add CLI args to 'nda-transform/count_assoc_files.py'
    Signed-off-by: Dheshan Mohandass <dheshan@mohandass.com>

--- nda-transform/count_assoc_files.py
+++ nda-transform/count_assoc_files.py
@@ -1,8 +1,78 @@
 #!/usr/bin/env python
+"""
+Counts unique and duplicate files for NDA submissions
+"""
 
+import argparse
 from pathlib import Path
+from typing import Dict, List, Literal, Set
+
 import pandas as pd
-import sys
+
+
+def count_files(
+    source_path: Path,
+    files: List[Path],
+    context: Literal["aws", "eris"],
+):
+    """
+    Count unique and duplicate files in the given list of files
+    """
+    files_count: int = 0
+    unique_files: Set = set()
+    duplicate_files_count: Dict[str, int] = {}
+
+    file_columns = (
+        "data_file1 data_file2 image_file bvalfile bvecfile transcript_file".split()
+    )
+
+    for file in files:
+        print(f"Checking {file}")
+
+        if context == "eris":
+            _data_file_path = source_path
+            df = pd.read_csv(file, header=1)
+        else:
+            nda_submissions_path = Path("/volumes/prod-ampscz/nda-submissions")
+            df = pd.read_csv(file)
+            _data_file_path = nda_submissions_path / df.loc[0]["SUBMISSION_FOLDER_NAME"]
+
+        # check a file only if at least one file_columns exist
+        check_file = False
+        for c in file_columns:
+            if c in df.columns:
+                check_file = True
+                break
+
+        if not check_file:
+            print("Expected columns not found in the file. Skipping.")
+            continue
+
+        for _, row in df.iterrows():
+            for c in file_columns:
+                if c in df.columns:
+                    if not pd.isna(row[c]):
+                        data_file = row[c]
+                        data_file_path: Path = _data_file_path / data_file
+
+                        if not data_file_path.exists():
+                            print(f"File does not exist: {data_file_path}")
+                        else:
+                            files_count += 1
+                            if data_file_path not in unique_files:
+                                unique_files.add(data_file_path)
+                            else:
+                                # count duplicates
+                                manifest = file.name
+                                if manifest in duplicate_files_count:
+                                    duplicate_files_count[manifest] += 1
+                                else:
+                                    duplicate_files_count[manifest] = 1
+
+    print(f"Total files: {files_count}")
+    print(f"Total unique files: {len(unique_files)}")
+    print(f"Duplicates: {duplicate_files_count}")
+
 
 # Usage:
 # at AWS workspace:
@@ -10,72 +80,54 @@ import sys
 # at ERIS cluster:
 # __file__ 0 0
 
-nda_submissions_path = Path("/volumes/prod-ampscz/nda-submissions")
-id = sys.argv[1]
-collection = sys.argv[2]
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Count unique and duplicate files")
+    parser.add_argument("-id", type=str, help="submission id", default="0")
+    parser.add_argument(
+        "-collection",
+        type=str,
+        help="""
+collection id:
+3705: prod-ampscz
+4366: prod-ampscz-pii
+0: eris
+""",
+        choices=["3705", "4366", "0"],
+        default="0",
+    )
+    args = parser.parse_args()
 
-if id=='0' or collection=='0':
-    source_path = Path('/data/predict1/to_nda/nda-submissions/network_combined')
-    files = source_path.glob(f'*csv')
-else:
-    source_path = Path(f"/volumes/prod-ampscz/collaboration-space/{collection}")
-    files = source_path.glob(f"*/csv/{id}/part-*csv")
+    submission_id = args.id
+    collection_id = args.collection
 
-file_columns='data_file1 data_file2 image_file bvalfile bvecfile transcript_file'.split()
+    print(f"Submission id: {submission_id}")
+    print(f"Collection id: {collection_id}")
 
-count=0
-unique_file={}
-duplicate={}
-for file in files:
-    print("Checking", file)
-
-    
-    if id=='0' or collection=='0':
-        _data_file_path = source_path
-        df = pd.read_csv(file, header=1)
+    if submission_id == "0" or collection_id == "0":
+        # ERIS
+        CONTEXT = "eris"
+        source_path = Path("/data/predict1/to_nda/nda-submissions/network_combined")
+        files = source_path.glob("*csv")
     else:
-        df = pd.read_csv(file)
-        _data_file_path = nda_submissions_path / df.loc[0]["SUBMISSION_FOLDER_NAME"]
+        # AWS
+        CONTEXT = "aws"
+        if collection_id == "3705":
+            source_path = Path(
+                f"/volumes/prod-ampscz/collaboration-space/{collection_id}"
+            )
+        elif collection_id == "4366":
+            source_path = Path(
+                f"/volumes/prod-ampscz-pii/collaboration-space/{collection_id}"
+            )
+        else:
+            raise ValueError(f"Unknown collection id: {collection_id}")
+        files = source_path.glob(f"*/csv/{submission_id}/part-*csv")
 
+    file_paths = list(files)
+    file_paths = sorted(file_paths)
 
-    # check a file only if at least one file_columns exist
-    exist=0
-    for c in file_columns:
-        if c in df.columns:
-            exist=1
-            break
-    if not exist:
-        continue
+    print(f"Context: {CONTEXT}")
+    print(f"Source path: {source_path}")
+    print(f"Total files: {len(file_paths)}")
 
-
-    for idx, row in df.iterrows():
-        
-        for c in file_columns:
-        
-            if c in df.columns:
-                if not pd.isna(row[c]):
-                    data_file = row[c]
-
-                    data_file_path: Path = _data_file_path / data_file
-
-                    if not data_file_path.exists():
-                        print(f"File does not exist: {data_file_path}")
-                    else:
-                        count+=1
-
-                        if data_file_path not in unique_file:
-                            unique_file[data_file_path]=1
-                        else:
-                            # count duplicates
-                            manifest=file.name
-                            if manifest in duplicate:
-                                duplicate[manifest]+=1
-                            else:
-                                duplicate[manifest]=1
-
-
-
-print('Total files', count)
-print('Total unique files', len(unique_file))
-print('Duplicates', duplicate)
-
+    count_files(source_path, file_paths, CONTEXT)
