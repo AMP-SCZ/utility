26d38de04616142edc200f5e44ef4a0e4d6975ad
Billah, Tashrif <35086881+tashrifbillah@users.noreply.github.com>
2024-04-01 21:34:11 -0400

    convenient script for detecting duplicate rows in NDA manifest

---
+++ nda-transform/detect_duplicate_rows.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+
+import sys
+
+with open(sys.argv[1]) as f:
+    lines=f.read().strip().split('\n')
+
+dict1={}
+
+for line in lines:
+    if line in dict1:
+        dict1[line]+=1
+    else:
+        dict1[line]=1
+
+print('Frequency <space> Line')
+for line in lines:
+    if dict1[line]>1:
+        print('{:2} {}'.format(dict1[line], line))
+        
