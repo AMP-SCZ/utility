a9446023c97629ae9d44454f1b064e50c17569f9
Billah, Tashrif <35086881+tashrifbillah@users.noreply.github.com>
2024-04-02 12:00:22 -0400

    * provide all csv files as args * include help message * print header only if there is duplicate in a file

--- nda-transform/detect_duplicate_rows.py
+++ nda-transform/detect_duplicate_rows.py
@@ -1,28 +1,38 @@
 #!/usr/bin/env python
 
 import sys
+from glob import glob
 
 if len(sys.argv)==1 or sys.argv[1] in ['-h','--help']:
-    print(f'Usage: {__file__} actirec01.csv')
+    print(f"""Usage:
+{__file__} actirec01.csv
+{__file__} /path/to/*csv""")
     exit()
 
-print('Checking', sys.argv[1])
 
-with open(sys.argv[1]) as f:
-    lines=f.read().strip().split('\n')
+files=sys.argv[1:]
+for file in files:
+    print('Checking', file)
 
-dict1={}
+    with open(file) as f:
+        lines=f.read().strip().split('\n')
 
-for line in lines:
-    if line in dict1:
-        dict1[line]+=1
-    else:
-        dict1[line]=1
+    dict1={}
 
-print(' # : Line')
-for line in dict1:
-    if dict1[line]>1:
-        print('{:2} : {}'.format(dict1[line], line))
+    duplicate=False
+    for line in lines:
+        if line in dict1:
+            dict1[line]+=1
+            duplicate=True
+        else:
+            dict1[line]=1
+    
+    if duplicate:
+        print(' # : Line')
 
-print()
-        
+    for line in dict1:
+        if dict1[line]>1:
+            print('{:2} : {}'.format(dict1[line], line))
+
+    print()
+    
