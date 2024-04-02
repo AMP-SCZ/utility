bed3dc3e7f813bb326146e94029e496f35980b88
Billah, Tashrif <35086881+tashrifbillah@users.noreply.github.com>
2024-04-01 22:06:27 -0400

    * add help message * print status messages
    Co-authored-by: Dheshan Monhandass <dmohandass@mgh.harvard.edu>

--- nda-transform/detect_duplicate_rows.py
+++ nda-transform/detect_duplicate_rows.py
@@ -2,6 +2,12 @@
 
 import sys
 
+if len(sys.argv)==1 or sys.argv[1] in ['-h','--help']:
+    print(f'Usage: {__file__} actirec01.csv')
+    exit()
+
+print('Checking', sys.argv[1])
+
 with open(sys.argv[1]) as f:
     lines=f.read().strip().split('\n')
 
@@ -17,4 +23,6 @@ print(' # : Line')
 for line in dict1:
     if dict1[line]>1:
         print('{:2} : {}'.format(dict1[line], line))
+
+print()
         
