20e187801f810b987c6671c26f774ab7c95fb928
Billah, Tashrif <35086881+tashrifbillah@users.noreply.github.com>
2024-05-28 00:58:25 +1000

    use variable instead of repeated full path

--- rpms_cron.sh
+++ rpms_cron.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 
 datestamp=$(date +"%d.%m.%Y")
-mkdir -p /mnt/prescient/one_day_backup/
-rm /mnt/prescient/one_day_backup/*
-cp /mnt/prescient/RPMS_incoming/*_${datestamp}.csv /mnt/prescient/one_day_backup/
+INCOMING=/var/lib/prescient/RPMS_incoming
+BACKUP=/var/lib/prescient/one_day_backup
 
-export PATH=$HOME/miniconda3/bin/:/mnt/prescient/utility/:$PATH
+mkdir -p $BACKUP
+rm ${BACKUP}/*
+cp ${INCOMING}/*_${datestamp}.csv ${BACKUP}/
 
-rename_RPMS_vars.py /mnt/prescient/RPMS_incoming/ && \
-replace_RPMS_values.py /mnt/prescient/RPMS_incoming/ && \
-rpms_psychs_partition.py /mnt/prescient/RPMS_incoming/
+export PATH=$HOME/miniconda3/bin/:/var/lib/prescient/utility/:$PATH
+
+rename_RPMS_vars.py $INCOMING && \
+replace_RPMS_values.py $INCOMING && \
+rpms_psychs_partition.py $INCOMING
 
