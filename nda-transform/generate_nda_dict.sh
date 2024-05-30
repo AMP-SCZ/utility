24a7477807f1561ae7d2823cf297fd1ed6187d4a
Billah, Tashrif <35086881+tashrifbillah@users.noreply.github.com>
2024-05-30 09:53:33 -0400

    wip generate_nda_dict.sh

--- nda-transform/generate_nda_dict.sh
+++ nda-transform/generate_nda_dict.sh
@@ -29,14 +29,59 @@ with open(sys.argv[1],'w') as f:
     for v in vars:
         f.write(v+'\n')
 
-""" > /tmp/generate_nda_dict.py
+""" > /tmp/generate_nda_list.py
 
 
 OUT=${TO_NDA}/nda_vars.txt
+python /tmp/generate_nda_list.py $OUT
+
+"""
+import pandas as pd
+from glob import glob
+import sys
+
+files=glob('*_definitions.csv')
+
+# load list of variables
+with open(sys.argv[1]) as f:
+    vars=f.read().strip().split()
+
+
+# create list of definitions
+dfall={}
+for file in files:
+    df=pd.read_csv(file,header=1)
+    name=file.split('_definitions.csv')[0]
+    dfall[name]=df
+    
+
+# now search for variables
+for v in vars:
+    for df in dfall:
+
+        found=0
+        for i,row in df.iterrows():
+            if row['Variable Name']==v:
+                # write it out
+                found=1
+                break
+            
+            elif v in ['Aliases']:
+                # write it out
+                found=1
+                break
+        
+        if found:
+            break
+
+""" > /tmp/generate_nda_dict.py
+
+cd ${TO_NDA}/nda-templates/
 python /tmp/generate_nda_dict.py $OUT
 
 
-cd ${TO_NDA}/nda-templates/
+: << COMMENT
+
 DICT=${OUT//txt/csv}
 rm $DICT
 for v in $(cat $OUT)
@@ -45,4 +90,6 @@ do
     grep -h -w "\"${v}\"," *_definitions.csv >> $DICT
     grep -h -w "${v}," *_definitions.csv >> $DICT
 done
+
+COMMENT
 
