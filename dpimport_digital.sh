d6407dea64571308502efbc6e434d2ac742996ec
Billah, Tashrif <35086881+tashrifbillah@users.noreply.github.com>
2024-06-07 16:23:00 -0400

    introduce force flag for erasing and then importing data

--- dpimport_digital.sh
+++ dpimport_digital.sh
@@ -4,13 +4,17 @@ export PATH=/data/predict1/mongodb-linux-x86_64-rhel70-4.4.6/bin:$PATH
 
 if [ -z $1 ] || [ ! -d $1 ]
 then
-    echo """./dpimport_phone.sh /path/to/nda_root/ VM
+    echo """$0 /path/to/nda_root/ VM
 Provide /path/to/nda_root/ and VM
 VM name examples:
     dpstage for dpstage.dipr.partners.org
     rc-predict for rc-predict.bwh.harvard.edu
     rc-predict-dev for rc-predict-dev.bwh.harvard.edu
-    It is the first part of the server name."""
+    It is the first part of the server name.
+
+This script does incremental import by default.
+Provide 1 at the end to erase existing data and do a fresh import:
+    $0 /path/to/nda_root VM 1"""
     exit
 else
     export NDA_ROOT=$1
@@ -18,7 +22,8 @@ fi
 
 source /data/predict1/utility/.vault/.env.${2}
 
-: << COMMENT
+if [ "$3" == 1 ]
+then
 # remove old data
 mongo --tls --tlsCAFile $state/ssl/ca/cacert.pem \
 --tlsCertificateKeyFile $state/ssl/mongo_client.pem \
@@ -41,7 +46,7 @@ mongodb://dpdash:$MONGO_PASS@$HOST:$PORT/dpdata?authSource=admin \
 --eval "assess=[\"dpgvail\",\"dppay\",\"dpaxty\"]" /data/predict1/utility/remove_assess.js
 
 echo ''
-COMMENT
+fi
 
 # import new data
 export PATH=/data/predict1/miniconda3/bin:$PATH
--- dpimport_files_status.sh
+++ dpimport_files_status.sh
@@ -4,13 +4,17 @@ export PATH=/data/predict1/mongodb-linux-x86_64-rhel70-4.4.6/bin:$PATH
 
 if [ -z $1 ] || [ ! -d $1 ]
 then
-    echo """./dpimport_files_status.sh /path/to/nda_root/ VM
+    echo """$0 /path/to/nda_root/ VM
 Provide /path/to/nda_root/ and VM
 VM name examples:
     dpstage for dpstage.dipr.partners.org
     rc-predict for rc-predict.bwh.harvard.edu
     rc-predict-dev for rc-predict-dev.bwh.harvard.edu
-    It is the first part of the server name."""
+    It is the first part of the server name.
+
+This script does incremental import by default.
+Provide 1 at the end to erase existing data and do a fresh import:
+    $0 /path/to/nda_root VM 1"""
     exit
 else
     export NDA_ROOT=$1
@@ -21,13 +25,15 @@ source /data/predict1/utility/.vault/.env.${2}
 echo Importing to mongodb://dpdash:MONGO_PASS@$HOST:$PORT
 echo ''
 
-: << COMMENT
+if [ "$3" == 1 ]
+then
 # delete old collections
 mongo --tls --tlsCAFile $state/ssl/ca/cacert.pem --tlsCertificateKeyFile $state/ssl/mongo_client.pem mongodb://dpdash:$MONGO_PASS@$HOST:$PORT/dpdata?authSource=admin --eval "assess=[\"flowcheck\",\"data_baseline\",\"data_month_2\"]" /data/predict1/utility/remove_assess.js
 
 # delete metadata
 mongo --tls --tlsCAFile $state/ssl/ca/cacert.pem --tlsCertificateKeyFile $state/ssl/mongo_client.pem mongodb://dpdash:$MONGO_PASS@$HOST:$PORT/dpdata?authSource=admin /data/predict1/utility/remove_metadata.js
-COMMENT
+echo ''
+fi
 
 # import new collections
 export PATH=/data/predict1/miniconda3/bin:$PATH
