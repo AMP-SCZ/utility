3d086a04ab34e35ac0c88be7d783c52ab1f1e337
Billah, Tashrif <35086881+tashrifbillah@users.noreply.github.com>
2024-08-10 06:42:00 +1000

    poll RPMS_incoming/ for completion of data dump

--- rpms_cron.sh
+++ rpms_cron.sh
@@ -3,12 +3,24 @@
 datestamp=$(date +"%d.%m.%Y")
 INCOMING=/var/lib/prescient/RPMS_incoming
 BACKUP=/var/lib/prescient/one_day_backup
+export PATH=$HOME/miniconda3/bin/:/var/lib/prescient/utility/:$PATH
+
+# poll INCOMING folder for *_datestamp.csv files
+# execute this workflow only when export is complete
+while [ 1 ]
+do
+    count=`ls ${INCOMING}/*_${datestamp}.csv | wc -l`
+    if [ $count -lt 76 ]
+    then
+        sleep 120
+    else
+        break
+    fi
+done
 
 mkdir -p $BACKUP
 rm ${BACKUP}/*
 cp ${INCOMING}/*_${datestamp}.csv ${BACKUP}/
-
-export PATH=$HOME/miniconda3/bin/:/var/lib/prescient/utility/:$PATH
 
 rename_RPMS_vars.py $INCOMING && \
 replace_RPMS_values.py $INCOMING && \
