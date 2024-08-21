1672ab8b88e01a63d491c18ddaf581e4eb0ec3c2
Billah, Tashrif <35086881+tashrifbillah@users.noreply.github.com>
2024-08-21 11:56:51 -0400

    use variable for conciseness

--- clean_down_shift.sh
+++ clean_down_shift.sh
@@ -13,15 +13,16 @@ fi
 TOKEN=$1
 
 export PATH=/data/predict1/miniconda3/bin/:/data/predict1/utility/:$PATH
+PHOENIX_PROTECTED=/data/predict1/data_from_nda/Prescient/PHOENIX/PROTECTED
 
 # clean duplicate arms
-clean_old_arm.py /data/predict1/data_from_nda/Prescient/PHOENIX/PROTECTED/ $TOKEN
+clean_old_arm.py $PHOENIX_PROTECTED $TOKEN
 
 # download REDCap JSONs
 down_mgb_redcap_records.py /data/predict1/data_from_nda/Prescient/PHOENIX/PROTECTED/ $TOKEN
-chgrp BWH-PREDICT-G /data/predict1/data_from_nda/Prescient/PHOENIX/PROTECTED/Prescient??/raw/*/surveys/???????.Prescient.json
+chgrp BWH-PREDICT-G ${PHOENIX_PROTECTED}/Prescient??/raw/*/surveys/???????.Prescient.json
 
 # shift their dates
-shift_redcap_dates.py /data/predict1/data_from_nda/Prescient/PHOENIX/PROTECTED/ "*/raw/*/surveys/*.Prescient.json" /data/predict1/utility/yale-real/CloneOfYaleRealRecords_DataDictionary_2024-04-16.csv
-chgrp BWH-PREDICT-G /data/predict1/data_from_nda/Prescient/PHOENIX/GENERAL/Prescient??/processed/*/surveys/???????.Prescient.json
+shift_redcap_dates.py $PHOENIX_PROTECTED "*/raw/*/surveys/*.Prescient.json" /data/predict1/utility/yale-real/CloneOfYaleRealRecords_DataDictionary_2024-04-16.csv
+chgrp BWH-PREDICT-G ${PHOENIX_PROTECTED/PROTECTED/GENERAL}/Prescient??/processed/*/surveys/???????.Prescient.json
 
