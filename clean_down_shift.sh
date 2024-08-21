c13535783cc5af98bd128b07ac3d721a1911cfa3
Billah, Tashrif <35086881+tashrifbillah@users.noreply.github.com>
2024-08-21 11:17:07 -0400

    explicitly chgrp of JSONs

--- clean_down_shift.sh
+++ clean_down_shift.sh
@@ -17,12 +17,11 @@ export PATH=/data/predict1/miniconda3/bin/:/data/predict1/utility/:$PATH
 # clean duplicate arms
 clean_old_arm.py /data/predict1/data_from_nda/Prescient/PHOENIX/PROTECTED/ $TOKEN
 
-# set umask and group to avoid permission issue for team members
-umask 007 && newgrp BWH-PREDICT-G
-
 # download REDCap JSONs
 down_mgb_redcap_records.py /data/predict1/data_from_nda/Prescient/PHOENIX/PROTECTED/ $TOKEN
+chgrp BWH-PREDICT-G /data/predict1/data_from_nda/Prescient/PHOENIX/PROTECTED/Prescient??/raw/*/surveys/???????.Prescient.json
 
 # shift their dates
 shift_redcap_dates.py /data/predict1/data_from_nda/Prescient/PHOENIX/PROTECTED/ "*/raw/*/surveys/*.Prescient.json" /data/predict1/utility/yale-real/CloneOfYaleRealRecords_DataDictionary_2024-04-16.csv
+chgrp BWH-PREDICT-G /data/predict1/data_from_nda/Prescient/PHOENIX/GENERAL/Prescient??/processed/*/surveys/???????.Prescient.json
 
