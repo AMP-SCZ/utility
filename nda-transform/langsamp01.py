eb1b4b0c1e83cb06154f8b98c241853a1858eb4a
Billah, Tashrif <35086881+tashrifbillah@users.noreply.github.com>
2024-12-12 17:40:15 -0500

    * add visit column * for some scripts, check first if ampscz_missing_spec is blank

--- nda-transform/langsamp01.py
+++ nda-transform/langsamp01.py
@@ -95,7 +95,8 @@ def populate():
 
     # get form specific variables
     df.at[row,'interview_date']=nda_date(interview_date)
-    
+    df.at[row,'visit']=event
+
     chric_consent_date=get_value('chric_consent_date',f'screening_arm_{arm}')
     months=months_since_consent(interview_date,chric_consent_date)
     df.at[row,'interview_age']=dfshared.loc[src_subject_id,'interview_age']+months
@@ -276,7 +277,7 @@ if __name__=='__main__':
     elif prefix=='chrpsychs_av':
         interview_type='psychs'
 
-    columns=['subjectkey','src_subject_id','interview_date','interview_age','sex']
+    columns=['subjectkey','src_subject_id','interview_date','interview_age','sex','visit']
     # run sheet vars
     run_sheet_vars=[]
     for c in definition.index:
