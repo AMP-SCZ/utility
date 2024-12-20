929a440f88d05792eab04a1437768662f9d7fdf0
Billah, Tashrif <35086881+tashrifbillah@users.noreply.github.com>
2024-12-20 12:10:55 -0500

    shift langsamp01 interview dates

--- nda-transform/_langsamp01.py
+++ nda-transform/_langsamp01.py
@@ -2,6 +2,7 @@
 
 import pandas as pd
 import sys
+from datetime import datetime, timedelta
 
 pro=pd.read_csv('/data/predict1/data_from_nda/Pronet/PHOENIX/PROTECTED/date_offset.csv')
 pre=pd.read_csv('/data/predict1/data_from_nda/Prescient/PHOENIX/PROTECTED/date_offset.csv')
@@ -16,10 +17,17 @@ df1['visit']= list(map(lambda x: x.split('_arm_')[0],df['redcap_event_name']))
 
 # transcripts are located at:
 # {network}/PHOENIX/GENERAL/{site}/processed/{subject}/interviews/{type}/transcripts/*_REDACTED.txt
+d=[None]*df.shape[0]
 t=[None]*df.shape[0]
 for i,row in df.iterrows():
-    t[i]='{}/interviews/{}/transcripts/{}'.format(row['src_subject_id'],row['interview_type'],row['transcript_file'])
+    src_subject_id=row['src_subject_id']
+    t[i]='{}/interviews/{}/transcripts/{}'.format(src_subject_id,row['interview_type'],row['transcript_file'])
 
+    shift=int(dfshift.loc[src_subject_id,'days'])
+    shifted_date=datetime.strptime(row['interview_date'],'%Y-%m-%d')+timedelta(days=shift)
+    d[i]=shifted_date.strftime('%m/%d/%Y')
+
+df1['interview_date']=d
 df1['transcript_file']=t
 df1['subjectkey']=''
 df1['sex']=''
