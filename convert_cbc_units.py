a56543136f6f7c124bfa266f6287579ac44958c1
Billah, Tashrif <35086881+tashrifbillah@users.noreply.github.com>
2025-01-03 10:03:20 +1100

    convert cbc units to that of ProNET Reference: the email thread CBC data issues (12/21/2024)

---
+++ convert_cbc_units.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+
+import pandas as pd
+import sys
+from glob import glob
+from os import getcwd, chdir
+
+if len(sys.argv)<2 or sys.argv[1] in ['-h','--help']:
+    print(f'''Usage:
+    {__file__} /mnt/prescient/RPMS_incoming/''')
+    exit(0)
+
+
+dir_bak=getcwd()
+chdir(sys.argv[1])
+
+file=glob('PrescientStudy_Prescient_cbc_with_differential_*.csv')[0]
+df=pd.read_csv(file,dtype=str)
+df1=df.copy()
+
+for c in 'chrcbc_hct chrcbc_hct_high chrcbc_hct_low'.split():
+    df1[c]=df[c].apply(lambda x: round(100*float(x),2))
+
+for c in 'chrcbc_hgb chrcbc_hgb_high chrcbc_hgb_low \
+chrcbc_mchc chrcbc_mchc_high chrcbc_mchc_low'.split():
+    df1[c]=df[c].apply(lambda x: round(1/10*float(x),2))
+
+
+df1.to_csv(file,index=False)
+
+chdir(dir_bak)
+
