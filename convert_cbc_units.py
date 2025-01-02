6eb087fb8031f2145981acd3b6f08ed5b327687d
Billah, Tashrif <35086881+tashrifbillah@users.noreply.github.com>
2025-01-03 10:08:37 +1100

    include status messages in convert_cbc_units.py

--- convert_cbc_units.py
+++ convert_cbc_units.py
@@ -10,6 +10,7 @@ if len(sys.argv)<2 or sys.argv[1] in ['-h','--help']:
     {__file__} /mnt/prescient/RPMS_incoming/''')
     exit(0)
 
+print("\nConvert PRESCIENT CBC form units to that of ProNET's\n")
 
 dir_bak=getcwd()
 chdir(sys.argv[1])
@@ -19,10 +20,12 @@ df=pd.read_csv(file,dtype=str)
 df1=df.copy()
 
 for c in 'chrcbc_hct chrcbc_hct_high chrcbc_hct_low'.split():
+    print(c)
     df1[c]=df[c].apply(lambda x: round(100*float(x),2))
 
 for c in 'chrcbc_hgb chrcbc_hgb_high chrcbc_hgb_low \
 chrcbc_mchc chrcbc_mchc_high chrcbc_mchc_low'.split():
+    print(c)
     df1[c]=df[c].apply(lambda x: round(1/10*float(x),2))
 
 
