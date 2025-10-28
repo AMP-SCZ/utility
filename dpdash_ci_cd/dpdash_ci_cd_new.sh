7c885ad67cb32cdc7139bb626795c3521a4e2096
Billah, Tashrif <35086881+tashrifbillah@users.noreply.github.com>
2025-10-28 15:53:38 -0400

    new dpdash_ci_cd script for Gnar company DPdash (not yet production ready)

---
+++ dpdash_ci_cd/dpdash_ci_cd_new.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+# activate a proper Python environment for dpimport outside this script
+
+# provides mongoimport
+export PATH=~/Downloads/mongodb-database-tools-rhel93-x86_64-100.13.0/bin/:$PATH
+
+# provides mongosh
+~/Downloads/mongodb-mongosh-2.5.8.x86_64/usr/bin/:$PATH
+
+# provides import.py
+export PATH=~/Downloads/dpimport/scripts:$PATH
+
+# data for configuration
+mongoimport --uri="mongodb://zhpysch18.mgb.org:27017/dpdmongo?directConnection=true&serverSelectionTimeoutMS=2000&appName=mongosh+2.3.8" --collection=charts charts_20250815.json
+mongoimport --uri="mongodb://zhpysch18.mgb.org:27017/dpdmongo?directConnection=true&serverSelectionTimeoutMS=2000&appName=mongosh+2.3.8" --collection=users users_20250815.json
+mongoimport --uri="mongodb://zhpysch18.mgb.org:27017/dpdmongo?directConnection=true&serverSelectionTimeoutMS=2000&appName=mongosh+2.3.8" --collection=configs configs_20250815.json
+
+# data from charts
+cd /data/predict1/data_from_nda/
+import.py -c $CONFIG "Pr*/PHOENIX/PROTECTED/Pr*/processed/*/surveys/??-*-form_informed_consent_run_sheet-day1to*.csv" && \
+import.py -c $CONFIG "Pr*/PHOENIX/PROTECTED/Pr*/processed/*/surveys/??-*-form_inclusionexclusion_criteria_review-day1to*.csv" && \
+import.py -c $CONFIG "Pr*/PHOENIX/PROTECTED/Pr*/processed/*/surveys/??-*-form_sociodemographics-day1to*.csv" && \
+import.py -c $CONFIG "formsdb/dpdash-recruit-filter/??-*-form_filters-day1to1.csv"
+
+# data for files status
+import.py -c $CONFIG combined_metadata.csv && \
+import.py -c $CONFIG "*_status/??-*-data_*-day1to1.csv" && \
+import.py -c $CONFIG "*_status/combined-??-data_*-day1to1.csv"
+
+# login and do whatever
+mongosh "mongodb://zhpysch18.mgb.org:27017/dpdmongo?directConnection=true&serverSelectionTimeoutMS=2000&appName=mongosh+2.3.8"
+
