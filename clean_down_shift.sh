286b36a46cdfa7b8e1d08a162e6d0e23dd8ad764
Billah, Tashrif <35086881+tashrifbillah@users.noreply.github.com>
2025-12-08 15:56:24 -0500

    introduce FORCE arg with RPMS clean_down_shift.sh

--- clean_down_shift.sh
+++ clean_down_shift.sh
@@ -4,13 +4,22 @@ if [ "$1" == "-h" ] || [ $# == 0 ]
 then
     echo """Usage:
 $0 1234567890
-Provide REDCap API token
+$0 1234567890 1
+Provide REDCap API token.
+The trailing 1 is for forced download and date shifting of all.
 """
     exit
 fi
 
 
 TOKEN=$1
+if [ "$2" == "1" ]
+then
+    # first value: number of processors, second value: force"
+    FORCE="12 1"
+else
+    FORCE=
+fi
 
 export PATH=/data/predict1/miniconda3/bin/:/data/predict1/utility/:$PATH
 PHOENIX_PROTECTED=/data/predict1/data_from_nda/Prescient/PHOENIX/PROTECTED
@@ -19,10 +28,10 @@ PHOENIX_PROTECTED=/data/predict1/data_from_nda/Prescient/PHOENIX/PROTECTED
 clean_old_arm.py $PHOENIX_PROTECTED $TOKEN
 
 # download REDCap JSONs
-down_mgb_redcap_records.py $PHOENIX_PROTECTED $TOKEN
+down_mgb_redcap_records.py $PHOENIX_PROTECTED $TOKEN $FORCE
 
 # shift their dates
-shift_redcap_dates.py $PHOENIX_PROTECTED "*/raw/*/surveys/*.Prescient.json" /data/predict1/utility/yale-real/*_DataDictionary_*.csv
+shift_redcap_dates.py $PHOENIX_PROTECTED "*/raw/*/surveys/*.Prescient.json" /data/predict1/utility/yale-real/*_DataDictionary_*.csv $FORCE
 
 # explicit permission change
 n=Prescient
