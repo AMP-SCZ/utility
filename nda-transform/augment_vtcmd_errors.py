14b6672f7d022c20fa307f209ff6ae21ac9ac298
Billah, Tashrif <35086881+tashrifbillah@users.noreply.github.com>
2025-12-08 15:50:49 -0500

    @cridgew13 pointed out that validation errors' RECORD-1 matches with NDA file @tashrifbillah verified it

--- nda-transform/augment_vtcmd_errors.py
+++ nda-transform/augment_vtcmd_errors.py
@@ -9,18 +9,21 @@ if sys.argv[1] in ['-h', '--help'] or len(sys.argv)<3:
     exit()
 
 
-dferror=pd.read_csv(sys.argv[1])
-
-FILE=dferror.loc[0,'FILE']
-df=pd.read_csv(FILE,header=1,dtype=str)
+df=pd.read_csv(sys.argv[1],header=1,dtype=str)
+dferror=pd.read_csv(sys.argv[2])
 
 dferror1=dferror.copy()
 dferror1['src_subject_id']=''
+dferror1['VALUE']=''
 
+# RECORD is 1-indexed
+# df is 0-indexed
+# RECORD=i matches with df.loc[i-1]
 for i,row in dferror.iterrows():
     ind=row['RECORD']-1
     dferror1.loc[i,'src_subject_id']= df.loc[ind,'src_subject_id']
+    dferror1.loc[i,'VALUE']= df.loc[ind,row['COLUMN']]
 
 dferror1.drop('FILE ID STATUS EXPIRATION_DATE'.split(),axis=1,inplace=True)
-dferror1.to_csv(sys.argv[1]+'.augmented',index=False)
+dferror1.to_csv(sys.argv[2]+'.augmented',index=False)
 
