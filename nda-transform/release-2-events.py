d502f4a6990db190e0db806750fdd91bd52b7af2
Billah, Tashrif <35086881+tashrifbillah@users.noreply.github.com>
2025-12-11 11:42:41 -0500

    * accept REDCap-like instrument designation as input * introduce generate argument * add a section for submit argument * increase event's width to 14 characters

--- nda-transform/release-2-events.py
+++ nda-transform/release-2-events.py
@@ -3,7 +3,19 @@
 import pandas as pd
 import sys
 
-devents=pd.read_csv('/data/predict1/to_nda/nda-submissions/release-2-events')
+if len(sys.argv)<2 or sys.argv[1] in ['-h','--help']:
+    print(f'''Usage:
+    {__file__} /data/predict1/to_nda/nda-submissions/release-4-events generate
+    {__file__} /data/predict1/to_nda/nda-submissions/release-4-events combine
+    {__file__} /data/predict1/to_nda/nda-submissions/release-2-events submit
+
+This script gives nicely formatted commands to include in release-?-events.sh.''')
+    exit(0)
+
+
+devents=pd.read_csv(sys.argv[1])
+# devents=pd.read_csv('/data/predict1/to_nda/nda-submissions/release-2-events')
+# devents=pd.read_csv('/data/predict1/to_nda/nda-submissions/release-4-events')
 # to group same form together
 # devents.sort_values('form',inplace=True)
 redcap_nda=pd.read_csv('/data/predict1/utility/nda-transform/redcap_nda_form_mapping.csv')
@@ -20,14 +32,17 @@ for i,row in devents.iterrows():
             addl_arg=_row['addl_arg']
             short_name=_row['nda_short_name']
             
-            if len(sys.argv)==1:
-                cmd=f'./generate.sh -n $n -e {event:9} -f {short_name:16} '
+            if sys.argv[2]=='generate':
+                cmd=f'./generate.sh -n $n -e {event:14} -f {short_name:16} '
                 
                 if not pd.isna(addl_arg):
                     cmd+=f'-p {addl_arg}'
                 
-            elif sys.argv[1]=='combine':
-                cmd=f'./combine_networks.sh -e {event:9} -f {short_name:16} '
+            elif sys.argv[2]=='combine':
+                cmd=f'./combine_networks.sh -e {event:14} -f {short_name:16} '
+
+            elif sys.argv[2]=='submit':
+                cmd=f'./combine_networks.sh -u tbillah -e {event:14} -f {short_name:16} '
 
             print(cmd)
             
