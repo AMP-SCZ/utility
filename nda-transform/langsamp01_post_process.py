ff223f35939770a948ab0fddded9b7d8c595eec3
Billah, Tashrif <35086881+tashrifbillah@users.noreply.github.com>
2026-01-25 19:09:12 -0500

    post processor for langsamp01 data After Dheshan's interview date and visit label insertion

---
+++ nda-transform/langsamp01_post_process.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python
+
+import pandas as pd
+import sys
+
+df=pd.read_csv(sys.argv[1], dtype=str)
+df1= df.copy()
+
+# rename columns
+df1.rename(columns={'file_name.txt':'transcript_file'}, inplace=True)
+
+# drop non-existent columns
+df1.drop(['n_words','n_disfluencies_per_word'], axis=1, inplace=True)
+
+# reshape interview dates to MM/DD/YYYY
+df1.interview_date= df.interview_date.apply(lambda x: f'{x[5:7]}/{x[8:10]}/{x[0:4]}')
+
+# make some columns integers
+for c in """n_disfluencies
+    n_false_starts
+    n_word_repeats
+    n_stutters
+    n_non_verbal_fillers
+    n_verbal_fillers""".split():
+    
+    df1[c]=df[c].apply(lambda x: str(int(float(x))) if not pd.isna(x) else '')
+
+
+# change open/psychs to 1/2
+df1.interview_type= df.interview_type.map(lambda x: 1 if x=='open' \
+    else 2 if x=='psychs' else '') 
+
+
+# trim _arm_? from visit label
+df1.visit= df.visit.map(lambda x: x.split('_arm_')[0])
+
+
+# keep integer only from session???
+df1.interview_number= df.interview_number.apply(lambda x: int(x[-3:]) if 'session' in x \
+    else int(x[-1]) if 'submission' in x else '')
+
+
+# change column order
+begin='subjectkey,src_subject_id,interview_date,interview_age,sex,visit'.split(',')
+for c in df1.columns:
+    if c not in begin:
+        begin.append(c)
+
+df2= df1[begin]
+# save the data back
+df2.to_csv(sys.argv[1].strip('.dm1447'), index=False)
+
+
