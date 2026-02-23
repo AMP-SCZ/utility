4ecb59cb4782df0144680ab33e8fdea9eb026a95
Billah, Tashrif <35086881+tashrifbillah@users.noreply.github.com>
2026-02-23 14:57:50 -0500

    * save a file based on REWRITE flag * activate JSON dump block * made two functions REPLACE and VERIFY * VERIFY block is similar to REPLACE

--- replace_figs_values.py
+++ replace_figs_values.py
@@ -4,7 +4,7 @@ import pandas as pd
 import json
 import sys
 from shutil import copyfile
-
+from os.path import basename
 
 if len(sys.argv)<3 or sys.argv[1] in ['-h','--help']:
     print(f'''Usage:
@@ -26,33 +26,68 @@ for i,row in ddict.iterrows():
             if v in df.columns:
                 vars.append(row['field_name'])
             else:
-                print(v)
+                pass
+                # print(v)
 
 
-for i,row in df.iterrows():
-    s= row['study_id']
-    site= s[:2]
 
-    file= f'/data/predict1/data_from_nda/Prescient/PHOENIX/GENERAL/Prescient{site}/processed/{s}/surveys/{s}.Prescient.json'
+def REPLACE():
+    for i,row in df.iterrows():
+        s= row['study_id']
+        site= s[:2]
 
-    print('Processing', file)
+        file= f'/data/predict1/data_from_nda/Prescient/PHOENIX/GENERAL/Prescient{site}/processed/{s}/surveys/{s}.Prescient.json'
 
-    # load json
-    with open(file) as f:
-        dict1=json.load(f)
+        # print('Processing', file)
 
-
-    # replace FIGS calc fields
-    for d in dict1:
-        if d['redcap_event_name']==row['redcap_event_name']:
-            for v in vars:
-                d[v]= row[v]
-
-        break
-
-    copyfile(file, file+'.bak')
-
-    #with open(file,'w') as f:
-    #    json.dump(dict1,f)
+        # load json
+        with open(file) as f:
+            dict1=json.load(f)
 
 
+        # replace FIGS calc fields
+        REWRITE=False
+        for d in dict1:
+            if d['redcap_event_name']==row['redcap_event_name']:
+                for v in vars:
+                    if d[v] != row[v]:
+                        REWRITE=True
+                        d[v]= row[v]
+
+            break
+
+        copyfile(file, file+'.bak')
+
+        if REWRITE:
+            print(file)
+            with open(file,'w') as f:
+                json.dump(dict1,f)
+
+
+def VERIFY():
+    for i,row in df.iterrows():
+        s= row['study_id']
+        site= s[:2]
+
+        file= f'/data/predict1/data_from_nda/Prescient/PHOENIX/GENERAL/Prescient{site}/processed/{s}/surveys/{s}.Prescient.json'
+
+        # print('Verifying', file)
+
+        # load json
+        with open(file) as f:
+            dict1=json.load(f)
+
+
+        # replace FIGS calc fields
+        REWRITE=False
+        for d in dict1:
+            if d['redcap_event_name']==row['redcap_event_name']:
+                for v in vars:
+                    if d[v] != row[v]:
+                        print(f'{basename(file)},{v},{d[v]},{row[v]}')
+
+            break
+
+
+REPLACE()
+# VERIFY()
