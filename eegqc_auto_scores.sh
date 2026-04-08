1b93775186409822db8e8d9a70956f4536455e97
Billah, Tashrif <35086881+tashrifbillah@users.noreply.github.com>
2026-04-08 11:57:14 -0400

    change how argument is supplied to script

--- eegqc_auto_scores.sh
+++ eegqc_auto_scores.sh
@@ -3,5 +3,5 @@
 NDA_ROOT=$1
 
 /data/predict1/miniconda3/bin/python /data/predict1/eeg-qc-dash/insert_auto_scores.py
-rsync -av $NDA_ROOT/.scores.pkl rc-predict-gen.partners.org:$2
+rsync -av $NDA_ROOT/.scores.pkl $2
 
