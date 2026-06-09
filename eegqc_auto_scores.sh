56b0f0ee3b6212f249263d1e8595ad8dc9ef4d09
Billah, Tashrif <35086881+tashrifbillah@users.noreply.github.com>
2026-06-09 15:23:05 -0400

    export rsync PATH, libraries explicitly

--- eegqc_auto_scores.sh
+++ eegqc_auto_scores.sh
@@ -3,6 +3,8 @@
 NDA_ROOT=$1
 
 /data/predict1/miniconda3/bin/python /data/predict1/eeg-qc-dash/insert_auto_scores.py
-module load librsync rsync
+
+export LD_LIBRARY_PATH=/apps/software/librsync/2.3.4-GCCcore-13.3.0/lib:$LD_LIBRARY_PATH
+export PATH=/apps/software/rsync/3.4.1-GCCcore-13.3.0/bin:$PATH
 rsync -av $NDA_ROOT/.scores.pkl $2
 
