0ca6202bc3a10269b39b770a65697c7d4f823ed3
Billah, Tashrif <35086881+tashrifbillah@users.noreply.github.com>
2026-06-09 15:48:54 -0400

    use -O --no-perms flags to bypass permission errors

--- rsync_files_status.sh
+++ rsync_files_status.sh
@@ -3,7 +3,7 @@
 export LD_LIBRARY_PATH=/apps/software/librsync/2.3.4-GCCcore-13.3.0/lib:$LD_LIBRARY_PATH
 export PATH=/apps/software/rsync/3.4.1-GCCcore-13.3.0/bin:$PATH
 
-rsync -avR /data/predict1/data_from_nda/combined-*-data_*-day1to1.csv rc-predict-dev.partners.org:/
-rsync -avR /data/predict1/data_from_nda/Pronet_status/combined-*-data_*-day1to1.csv rc-predict-dev.partners.org:/
-rsync -avR /data/predict1/data_from_nda/Prescient_status/combined-*-data_*-day1to1.csv rc-predict-dev.partners.org:/
+rsync -avR -O --no-perms /data/predict1/data_from_nda/combined-*-data_*-day1to1.csv zhpysch13.mgb.org:/
+rsync -avR -O --no-perms /data/predict1/data_from_nda/Pronet_status/combined-*-data_*-day1to1.csv zhpysch13.mgb.org:/
+rsync -avR -O --no-perms /data/predict1/data_from_nda/Prescient_status/combined-*-data_*-day1to1.csv zhpysch13.mgb.org:/
 
