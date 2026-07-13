b9dfb7b9e66729e2decd3057e3bacb93cb3c12c9
Billah, Tashrif <35086881+tashrifbillah@users.noreply.github.com>
2026-07-13 16:05:33 -0400

    PRESCIENT REDCap jobs moved to sf284

--- _crontab.sh
+++ _crontab.sh
@@ -3,21 +3,25 @@
 
 
 
-# erishpc-login-001
+# === erishpc-login-001 ===
+# as sf284
 
 # PRESCIENT real
 # determine if new and upload to REDCap
-0 18 * * 0,3 /data/predict1/miniconda3/bin/python /data/predict1/utility/set_rpms_date_shifts.py /data/predict1/data_from_nda/Prescient/PHOENIX/PROTECTED/ "*/raw/???????/surveys/" && /data/predict1/utility/_rpms_to_redcap.sh /data/predict1/data_from_nda/Prescient/PHOENIX/PROTECTED /data/predict1/utility/yale-real 123456
+0 18 * * 0 /data/predict1/miniconda3/bin/python /data/predict1/utility/set_rpms_date_shifts.py /data/predict1/data_from_nda/Prescient/PHOENIX/PROTECTED/ "*/raw/???????/surveys/" && /data/predict1/utility/_rpms_to_redcap.sh /data/predict1/data_from_nda/Prescient/PHOENIX/PROTECTED /data/predict1/utility/yale-real 123456
 
-# keep 12 hours difference between upload and download so upload can complete
+# keep 24 hours difference between upload and download so upload can complete
 
 # clean old arms, download JSONs from REDCap and shift their dates
-0 6 * * 1,4 /data/predict1/utility/clean_down_shift.sh 123456
+0 6 * * 2 /data/predict1/utility/clean_down_shift.sh 123456
 
 # kill stale processes every Saturday
 0 17 * * 6 pkill -u tb571 python
 
-# === dn018 ===
+
+
+# === erishpc-login-002 ===
+# as sf284
 
 # ProNET real
 # determine if new and shift dates
